eddaff01397ee2417867dc74b29b9b1af336c702
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2023-05-11 16:36:25 +0530

    added quiz module to pre app

--- src/app/pages/pre-application/list-modules/list-modules.component.ts
+++ src/app/pages/pre-application/list-modules/list-modules.component.ts
@@ -3,6 +3,7 @@ import {PreAppService} from "../pre-app.service";
 import {Observable} from "rxjs";
 import {SubModuleList} from "../../../@Models/pre-application.model";
 import {Router} from "@angular/router";
+import {MenuItem} from "primeng/api";
 
 @Component({
     selector: 'uni-list-modules',
@@ -13,14 +14,75 @@ export class ListModulesComponent implements OnInit {
     subModules$!: Observable<SubModuleList[]>;
     selectedSubModule: any;
     subModuleList: any[] = [];
+    isStartQuiz: boolean = false;
+    isQuizSubmit: boolean = false;
+    responsiveOptions: any[] = [];
+    quizData: any [] = [];
+    selectedQuestion: number = 0;
+    positionNumber: number = 0;
+    breadCrumb: MenuItem[] = [];
 
     constructor(private preAppService: PreAppService, private router: Router) {
+        this.responsiveOptions = [
+            {
+                breakpoint: '1199px',
+                numVisible: 1,
+                numScroll: 1
+            },
+            {
+                breakpoint: '991px',
+                numVisible: 2,
+                numScroll: 1
+            },
+            {
+                breakpoint: '767px',
+                numVisible: 1,
+                numScroll: 1
+            }
+        ];
     }
 
     ngOnInit(): void {
+        this.quizData = [{
+            question: "What is the name of the company?",
+            answer: ["Uni Abroad", "Uni Prep", "Uni SOP"]
+        },{
+            question: "what is status of the application",
+            answer: ["Processed", "Hold", "Rejected", "Accepted"]
+        }]
         this.subModules$ = this.preAppService.subModuleList$();
         let countryId = Number(localStorage.getItem('countryId'));
         this.preAppService.loadSubModules(countryId);
+    }
+    clickPreviousQuiz(carousel: any, event: any){
+
+    }
+
+    clickNextQuiz(carousel: any, event: any){
+
+    }
+
+    clickSubmitQuiz(event: any){
+        this.isStartQuiz = false;
+        this.isQuizSubmit = true;
+    }
+
+    startQuiz() {
+        this.isStartQuiz = true;
+    }
+    goToHome(event: any){
+
+    }
+
+    setPage(page: any) {
+        let pageNum: number = 0
+        if (page.page < 0) {
+            pageNum = this.quizData.length;
+        } else {
+            pageNum = page.page
+        }
+        this.positionNumber = pageNum + 1;
+        this.breadCrumb = [{label: 'POST Application'}, {label: 'test modules'}, {label: `Question ${pageNum + 1}`}];
     }
 
     onSubModuleClick(id: any) {
--- src/app/pages/pre-application/question-list/question-list.component.ts
+++ src/app/pages/pre-application/question-list/question-list.component.ts
@@ -1,11 +1,11 @@
-import { AfterContentChecked, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Observable } from "rxjs";
-import { PreAppService } from "../pre-app.service";
-import { ListQuestion } from "../../../@Models/question-list.model";
-import { MenuItem } from "primeng/api";
-import { DataService } from 'src/app/data.service';
-import { ActivatedRoute } from "@angular/router";
-import { SubModuleList } from "../../../@Models/pre-application.model";
+import {AfterContentChecked, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {Observable} from "rxjs";
+import {PreAppService} from "../pre-app.service";
+import {ListQuestion} from "../../../@Models/question-list.model";
+import {MenuItem} from "primeng/api";
+import {DataService} from 'src/app/data.service';
+import {ActivatedRoute} from "@angular/router";
+import {SubModuleList} from "../../../@Models/pre-application.model";
 
 @Component({
     selector: 'uni-question-list',
@@ -33,12 +33,10 @@ export class QuestionListComponent implements OnInit, AfterContentChecked {
     subModuleId: any;
     videoLinks: any;
     refLink: any;
-    imageLink: any;
     countryId: any;
-    test: any[] = [];
 
     constructor(private preAppService: PreAppService, private changeDetector: ChangeDetectorRef,
-        private dataService: DataService, private route: ActivatedRoute) {
+                private dataService: DataService, private route: ActivatedRoute) {
     }
 
     ngAfterContentChecked(): void {
@@ -54,7 +52,7 @@ export class QuestionListComponent implements OnInit, AfterContentChecked {
         this.getSubmoduleName(this.countryId);
 
         this.dataService.currentMessage.subscribe(message => this.message = message)
-        this.breadCrumb = [{ label: 'Pre Application' }, { label: this.moduleName }, { label: 'Question' }];
+        this.breadCrumb = [{label: 'Pre Application'}, {label: this.moduleName}, {label: 'Question'}];
 
         this.responsiveOptions = [
             {
@@ -100,9 +98,9 @@ export class QuestionListComponent implements OnInit, AfterContentChecked {
         this.listQuestion$.subscribe(event => {
             this.data = event
         });
-        this.selectedQuestion = id-1;
+        this.selectedQuestion = id - 1;
         this.positionNumber = id;
-        this.breadCrumb = [{ label: 'Pre Application' }, { label: this.moduleName }, { label: `Question ${id}` }];
+        this.breadCrumb = [{label: 'Pre Application'}, {label: this.moduleName}, {label: `Question ${id}`}];
         this.isQuestionAnswerVisible = true;
 
     }
@@ -115,57 +113,33 @@ export class QuestionListComponent implements OnInit, AfterContentChecked {
             pageNum = page.page
         }
         this.positionNumber = pageNum + 1;
-        this.breadCrumb = [{ label: 'Pre Application' }, { label: this.moduleName }, { label: `Question ${pageNum + 1}` }];
+        this.breadCrumb = [{label: 'Pre Application'}, {label: this.moduleName}, {label: `Question ${pageNum + 1}`}];
     }
 
     clickPrevious(carousel: any, event: any) {
-        this.selectedQuestion = this.selectedQuestion - 1;
-        console.log('prev',this.selectedQuestion);
         if (this.selectedQuestion <= 0) {
             return;
         }
+        this.selectedQuestion = this.selectedQuestion - 1;
         this.data.filter((res: any) => {
-            if (res.id == this.selectedQuestion) {
-                    this.refLink = res.reflink;
-                } else {
-                    this.refLink = [];
-            }
-        })
-        // this.data.filter((res: any) => {
-        //     if (res.id == this.selectedQuestion) {
-        //         this.videoLinks = res.videolink;
-        //         }
-        // })
-        this.data.filter((res: any) => {
-            if (res.id == this.selectedQuestion) {
+            if (res.id == this.selectedQuestion + 1) {
+                this.refLink = res.reflink;
                 this.videoLinks = res.videolink;
             }
-        })
-        this.data.find((res: any) => {
-            res.id == this.selectedQuestion ? this.videoLinks = res.videolink : this.videoLinks = [];
         })
         carousel.navBackward(event, this.selectedQuestion)
     }
 
     clickNext(carousel: any, event: any) {
-        this.selectedQuestion = this.selectedQuestion + 1;
-        console.log('next', this.selectedQuestion)
         if (this.selectedQuestion > this.data.length) {
             return;
         }
+        this.selectedQuestion = this.selectedQuestion + 1;
         this.data.filter((res: any) => {
-            console.log(res);
-            if (res.id == this.selectedQuestion) {
-                    this.refLink = res.reflink;
-                } else {
-                    this.refLink = [];
+            if (res.id == this.selectedQuestion + 1) {
+                this.refLink = res.reflink;
+                this.videoLinks = res.videolink;
             }
-        })
-        // this.data.find((res: any) => {
-        //     res.id == this.selectedQuestion ? this.refLink = res.reflink : this.refLink = [];
-        // })
-        this.data.find((res: any) => {
-            res.id == this.selectedQuestion ? this.videoLinks = res.videolink : this.videoLinks = [];
         })
         carousel.navForward(event, this.selectedQuestion)
     }
@@ -191,13 +165,20 @@ export class QuestionListComponent implements OnInit, AfterContentChecked {
     }
 
     clickPreviousRef(event: any) {
-        if (this.selectedQuestion < 2) {
-            this.selectedQuestion = this.data.length;
-            this.carouselRefElm.navBackward(event, this.selectedQuestion);
+        // if (this.selectedQuestion < 2) {
+        //     this.selectedQuestion = this.data.length;
+        //     this.carouselRefElm.navBackward(event, this.selectedQuestion);
+        //     return;
+        // }
+        //
+        // this.carouselRefElm.navBackward(event, --this.selectedQuestion)
+        if (this.selectedVideo <= 0) {
             return;
         }
+        this.selectedVideo = this.selectedVideo - 1;
+        console.log(this.selectedVideo)
 
-        this.carouselRefElm.navBackward(event, --this.selectedQuestion)
+        this.carouselVideoElm.navBackward(event, this.selectedVideo)
     }
 
     clickNextRef(event: any) {
@@ -206,7 +187,6 @@ export class QuestionListComponent implements OnInit, AfterContentChecked {
             this.carouselRefElm.navForward(event, this.selectedQuestion++)
             return;
         }
-        console.log(this.data)
         this.data.filter((res: any) => {
             if (res.id == this.selectedQuestion) {
                 console.log('res', res)
