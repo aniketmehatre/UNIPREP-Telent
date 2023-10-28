1eca2f79f6ace4110ce8b8682a403bd0a7c39bf8
Charlie legoriya <charlielegoriyagg>
2023-10-28 21:21:46 +0530

    subacription popup added

--- src/app/Auth/auth.guard.ts
+++ src/app/Auth/auth.guard.ts
@@ -12,14 +12,20 @@ import {
 import { Observable } from 'rxjs';
 import {LocalStorageService} from 'ngx-localstorage';
 import { JwtHelperService } from "@auth0/angular-jwt";
+import { DataService } from '../data.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanLoad {
+  min!: any;
+  userData: any;
   constructor(
     private storage: LocalStorageService,
-    private router: Router
+    private router: Router,
+    private dataService: DataService,
+    private authService: AuthService
   ) {}
 
   canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
@@ -35,6 +41,21 @@ export class AuthGuard implements CanActivate, CanLoad {
       const token = this.storage.get<string>('token') || '123';
       const helper = new JwtHelperService();
       isExpired = helper.isTokenExpired(token);
+      this.dataService.showTimerSource.subscribe((data) => {
+        if (data == "EXPIRED") {
+          this.min = 0;
+          return;
+        }
+        if (data) {
+          data = data.split("-", 4);
+          this.min = data[2];
+          let count = this.authService._userLoginCount;
+          if (this.min === "1" && count === 4) {
+            this.dataService.showTimeOut(true);
+          }
+        }
+      });
+
     } catch (e) { isExpired = true; }
     if (!isExpired) {
       return true;
