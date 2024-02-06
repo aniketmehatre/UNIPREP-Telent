0fa6a99fc37bd74c884cbb4e9f0890eb4ab6c0f1
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2024-02-06 10:48:17 +0530

    param added for staging
    param added for staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 const devdomain = 'api.uniprep.ai';
+const staging='staging.uniprep.ai'
 const proddomain='uniprep.ai'
 const devurl= `https://${devdomain}/uniprepapi/public/api`;
+const stagingurl=`https://${staging}/uniprepapi/public/api`;
 const produrl=`https://${proddomain}/uniprepapi/public/api`;
 export const environment = {
-  domain:devdomain,
+  domain:staging,
   production: false,
-  ApiUrl:devurl,
+  ApiUrl:stagingurl,
   tokenKey: 'token',
   facebookAppId: '892925195633254'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,14 @@
 const devdomain = 'api.uniprep.ai';
+const staging='staging.uniprep.ai'
 const proddomain='uniprep.ai'
 const devurl= `https://${devdomain}/uniprepapi/public/api`;
+const stagingurl=`https://${staging}/uniprepapi/public/api`;
 const produrl=`https://${proddomain}/uniprepapi/public/api`;
 
 export const environment = {
-  domain:devdomain,
+  domain:staging,
   production: false,
-  ApiUrl:devurl,
+  ApiUrl:stagingurl,
   tokenKey: 'token',
   facebookAppId: '892925195633254'
 };
