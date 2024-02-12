1bb59ebfd96ee6559b00805a147bf1de242846a3
Tamilselvan1996 <tamil@uniabroad.co.in>
2024-02-12 23:22:16 +0530

    ui fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@ const devurl= `https://${devdomain}/uniprepapi/public/api`;
 const stagingurl=`https://${staging}/uniprepapi/public/api`;
 const produrl=`https://${proddomain}/uniprepapi/public/api`;
 export const environment = {
-  domain:proddomain,
+  domain:devdomain,
   production: false,
-  ApiUrl:produrl,
+  ApiUrl:devurl,
   tokenKey: 'token',
   facebookAppId: '892925195633254',
   linkedinId: '86b55xx7n8zomc'
