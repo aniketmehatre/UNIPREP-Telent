29f5c03af22e30063933c94742641cbf27310be0
sreenivasan-frudev <130446246+sreenivasan-frudev@users.noreply.github.com>
2024-03-01 20:42:23 +0530

    changes

--- src/app/pages/user-guide/user-guide.component.ts
+++ src/app/pages/user-guide/user-guide.component.ts
@@ -20,6 +20,9 @@ export class UserGuideComponent implements OnInit {
 
   ngOnInit(): void {
     this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://api.uniprep.ai/uniprepapi/storage/app/public/UniprepUserGuide/userguidealltopics.pdf');
+    let iframeElement:any=document.getElementById("pdfIframe");
+    iframeElement.src="https://api.uniprep.ai/uniprepapi/storage/app/public/UniprepUserGuide/userguidealltopics.pdf#page=1&zoom=100";
+
   }
 
   changePdf(item: any) {
