92bb5fb3106d2c0a26965db7b40f00e75b97d673
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2024-03-02 11:56:32 +0530

    env change
    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ const stagingurl=`https://${staging}/uniprepapi/public/api`;
 const produrl=`https://${proddomain}/uniprepapi/public/api`;
 
 export const environment = {
-  domain:proddomain,
+  domain:devdomain,
   production: false,
-  ApiUrl:produrl,
+  ApiUrl:devurl,
   tokenKey: 'token',
   facebookAppId: '892925195633254',
   linkedinId: '86b55xx7n8zomc'
