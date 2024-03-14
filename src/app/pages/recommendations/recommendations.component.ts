f875555c4125e270baf595d43005a4ec80e47422
vivekuniabroad <139766038+vivekuniabroad@users.noreply.github.com>
2024-03-14 19:30:19 +0530

    recommendation basic fixes

--- src/app/pages/recommendations/recommendations.component.ts
+++ src/app/pages/recommendations/recommendations.component.ts
@@ -15,28 +15,29 @@ interface Provider {
   styleUrls: ['./recommendations.component.scss']
 })
 export class RecommendationsComponent implements OnInit {
-  selectedData: string[] = [];
-  currentTab = 0
+  selectedData: { [key: string]: boolean } = {};
+  activePageIndex:number = 0;
+  invalidClass:boolean = false;
   products = [
     {
       id: '1',
       question: 'Are you a student looking to study abroad?',
-      user_selection: true
+      // user_selection: true
     },
     {
       id: '2',
       question: 'Are you a job seeker?',
-      user_selection: false
+      // user_selection: false
     },
     {
       id: '3',
       question: 'Are you an Entrepreneur or looking to become one?',
-      user_selection: false
+      // user_selection: false
     },
     {
       id: '4',
       question: 'Are you looking to travel abroad?',
-      user_selection: false
+      // user_selection: false
     }
   ]
   recommended!: any;
@@ -56,31 +57,55 @@ export class RecommendationsComponent implements OnInit {
     })
   }
 
-  previous(product: any): void {
-    console.log(product)
+  previous(productId: number): void {
+    console.log(this.selectedData);
+    this.invalidClass = false;
+    if (productId in this.selectedData) {
+      if (this.activePageIndex > 0) {
+        this.activePageIndex--; // Decrement the active page index if it's not the first page
+      }
+    }else{
+      this.invalidClass = true;
+    }
   }
 
-  next(product: any): void {
-    console.log(product)
+  next(productId: number): void {
+    console.log(this.selectedData);
+    this.invalidClass = false;
+    if (productId in this.selectedData) {
+      if (this.activePageIndex < this.products.length - 1) {
+        this.activePageIndex++;
+      }
+    }else{
+      this.invalidClass = true;
+    }
   }
 
-  getRecommendation(product: any): void{
-    console.log(product);
-    this.enableModule = true;
+  getRecommendation(productId: any): void{
+    this.invalidClass = false;
+    if (productId in this.selectedData) {
+      this.enableModule = true;
+    }else{
+      this.invalidClass = true;
+    }
+    console.log(this.activePageIndex);
+    console.log(this.selectedData);
+    console.log(this.products.length - 1);
   }
 
   resetRecommendation(): void{
     this.enableModule = false;
+    this.selectedData = {};
+    console.log(this.selectedData);
   }
 
-  onChange(data:any , id:any) {
-    const latestCity = this.selectedData[this.selectedData.length - 1];
-    this.selectedData.length = 0;
-    this.selectedData.push(latestCity);
-    this.products.forEach(x => {
-      if (x.id === id) {
-        x.user_selection = data;
-      }
-    })
+  onChange() {
+    
+    console.log(this.selectedData);
+
+  }
+
+  onClickRadioButton(){
+    this.invalidClass = false;
   }
 }
