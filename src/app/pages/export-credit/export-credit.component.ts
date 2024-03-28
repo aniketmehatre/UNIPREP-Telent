09d4b12993821232da4edcde5d7b38a5c70c9d68
vivekuniabroad <139766038+vivekuniabroad@users.noreply.github.com>
2024-03-28 16:29:32 +0530

    export credit subscription plan

--- src/app/pages/export-credit/export-credit.component.ts
+++ src/app/pages/export-credit/export-credit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ExportCreditService } from './export-credit.service';
+import { AuthService } from 'src/app/Auth/auth.service';
 
 @Component({
   selector: 'uni-export-credit',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExportCreditComponent implements OnInit {
   value3:number = 0;
-  constructor() { }
+  moduleList:any[] = [];
+  planName: string = "";
+  constructor(private exportcreditservice:ExportCreditService,private authService: AuthService, ) { }
 
   ngOnInit(): void {
+    this.checkSubscriptionPlan();
+    this.loadModuleList();
+  }
+
+  checkSubscriptionPlan(){
+    this.authService.getNewUserTimeLeft().subscribe((res) => {
+      console.log(res);
+      this.planName = res.subscription_plan;
+    })
+  }
+
+  loadModuleList(){
+    this.exportcreditservice.getModulesList().subscribe((responce) =>{
+      console.log(responce);
+      this.moduleList = responce;
+    })
   }
 
 }
