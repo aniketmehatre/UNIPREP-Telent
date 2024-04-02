431603ce4a5cd19de747394f547c46db31f38411
vivekuniabroad <139766038+vivekuniabroad@users.noreply.github.com>
2024-04-02 19:28:19 +0530

    buy credits Subscription payment gatway

--- src/app/pages/export-credit/export-credit.component.ts
+++ src/app/pages/export-credit/export-credit.component.ts
@@ -1,77 +1,118 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { ExportCreditService } from './export-credit.service';
 import { AuthService } from 'src/app/Auth/auth.service';
-
+import { MessageService } from 'primeng/api';
+import { environment } from '@env/environment';
+import { WindowRefService } from '../subscription/window-ref.service';
+import { ResponsiveCSSClassPipe } from 'ngx-extended-pdf-viewer';
 @Component({
   selector: 'uni-export-credit',
   templateUrl: './export-credit.component.html',
   styleUrls: ['./export-credit.component.scss']
 })
 export class ExportCreditComponent implements OnInit {
-  creditValue: { [moduleId: string]: number } = {};
+ 
   moduleList:any[] = [];
-  planName: string = "";
-  firstArray:any[] = [];
-  couponInput:string = "";
   totalPayableAmount:number = 0;
   @Output() pay = new EventEmitter();
 
-  constructor(private exportcreditservice:ExportCreditService,private authService: AuthService, ) { }
+  constructor(private exportcreditservice:ExportCreditService,private authService: AuthService, private toastr:MessageService, private winRef: WindowRefService) { }
 
   ngOnInit(): void {
-    this.checkSubscriptionPlan();
     this.loadModuleList();
   }
 
-  checkSubscriptionPlan(){
-    this.authService.getNewUserTimeLeft().subscribe((res) => {
-      // this.planName = res?.subscription_details?.subscription_plan;
-      this.planName = res.subscription_details.subscription_plan;
-        console.log(this.planName);
-    })
-  }
-
   loadModuleList(): void{
-    
     this.exportcreditservice.getModulesList().subscribe((responce) =>{
-      this.firstArray = responce;
-      this.firstArray.forEach(item => {
-        this.creditValue[item.id] = 0;
-        if(this.planName == "Student"){
-          if(item.id == 3){
-            item.planValidation = 1;
-          }else{
-            item.planValidation = 0;
-          }
-        }else if(this.planName == "Career"){
-          if(item.id == 2 || item.id == 3){
-            item.planValidation = 1;
-          }else{
-            item.planValidation = 0;
-          }
-        }else if(this.planName == "Entrepreneur"){
-          item.planValidation = 1;
-        }
-        item.addedCredits = 0;
-        item.added_credit_rupees = 0;
-      });
-      this.moduleList = this.firstArray;
-      console.log(this.moduleList);
-    })
+      this.moduleList = responce;
+    });
+    console.log(this.moduleList);
   }
 
+  
   checkOut(){
     this.updateCredits();
-    // this.moduleList.push({total_amount: this.totalPayableAmount})
-    console.log(this.moduleList);
-    // let OrderDatas:any = [];
-    // OrderDatas = this.creditValue; 
-    // OrderDatas.push({ total_amount: this.totalPayableAmount });
-    // console.log(OrderDatas);
+    if(this.totalPayableAmount == 0){
+      this.toastr.add({severity:"error", summary: "error", detail: "Please add some Credits"});
+      return;
+    }
     this.exportcreditservice.placeOrder(this.moduleList).subscribe((response)=>{
       console.log(response);
+      this.payWithRazor(response);
     });
     
+  }
+
+  payWithRazor(orderDetails: any) {
+    let razorKey = "rzp_live_YErYQVqDIrZn1D";
+    if (environment.domain == "api.uniprep.ai") {
+      razorKey = "rzp_test_Crpr7YkjPaCLEr";
+    }
+    const options: any = {
+      key: razorKey,
+      amount: orderDetails.final_amount * 100, // amount should be in paise format to display Rs 1255 without decimal point
+      currency: "INR",
+      name: "UNIPREP", // company name or product name
+      description: "UNIPREP Subscription", // product description
+      image: "https://uniprep.ai/uniprep-assets/images/icon-light.svg", // company logo or product image
+      order_id: orderDetails.order_id, // order_id created by you in backend
+
+      // prefill: {
+      //   name: this.selectedSubscription?.subscription,
+      //   email: this.authservice.user?.email,
+      //   contact: this.authservice.user?.usertype_name,
+      // },
+      notes: {
+        address:
+          " 165/1,Opp Brahmasthana Kalyana Mantapa Sahukar Chenniah Road, TK Layout, Mysuru - 570023",
+      },
+      modal: {
+        // We should prevent closing of the form when esc key is pressed.
+        escape: false,
+      },
+      theme: {
+        color: "#3f4c83",
+      },
+    };
+
+    options.handler = (response: any, error: any) => {
+      options.response = response;
+      var paymentdata = {
+        order_id: response?.razorpay_order_id,
+        payment_id: response?.razorpay_payment_id,
+        update_order_ids: orderDetails?.added_credit_ids,
+      };
+      setTimeout(() => {
+        this.exportcreditservice.completePayment(paymentdata).subscribe(
+          (response: any)=>{
+            this.toastr.add({
+              severity: response.status,
+              summary: response.status,
+              detail: response.message,
+            });
+            console.log(response);
+            //window.location.reload();
+          },(error: any)=>{
+            this.toastr.add({
+              severity: response.status,
+              summary: response.status,
+              detail: response.message,
+            });
+            console.log(error);
+            //window.location.reload();
+          }
+        );
+      }, 0);
+    };
+    options.modal.ondismiss = () => {
+      this.toastr.add({
+        severity: "error",
+        summary: "Error",
+        detail: "Transaction cancelled",
+      });
+    };
+    const rzp = new this.winRef.nativeWindow.Razorpay(options);
+    rzp.open();
   }
 
   updateCredits(){
@@ -83,7 +124,5 @@ export class ExportCreditComponent implements OnInit {
         this.totalPayableAmount += item.added_credit_rupees;
       }
     })
-    console.log(this.moduleList);
-    console.log(this.totalPayableAmount);
   }
 }
