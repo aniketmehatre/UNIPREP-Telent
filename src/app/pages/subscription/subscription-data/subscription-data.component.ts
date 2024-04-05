fe51fd4a6c5820f6a8dc22737c8db6c2fda56fcd
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2024-04-05 13:03:25 +0530

    fix for prod
    fix for prod

--- src/app/pages/subscription/subscription-data/subscription-data.component.ts
+++ src/app/pages/subscription/subscription-data/subscription-data.component.ts
@@ -267,7 +267,7 @@ export class SubscriptionDataComponent implements OnInit {
             couponApplied: this.iscouponReadonly ? 1 : 0,
             coupon: this.iscouponReadonly ? this.couponInput : '',
             coupon_id: this.usedCouponId,
-            subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
+            //subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
           }
           if (this.checkoutTotal == '') {
             data.finalPrice = this.subscriptionTotal;
@@ -282,7 +282,7 @@ export class SubscriptionDataComponent implements OnInit {
               finalPrice: this.checkoutTotal,
               couponApplied: this.couponInput ? 1 : 0,
               coupon: this.couponInput,
-              subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
+              //subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
             }
             this.subscriptionPlan.emit(data);
           }
@@ -299,7 +299,7 @@ export class SubscriptionDataComponent implements OnInit {
             finalPrice: this.checkoutTotal,
             couponApplied: this.couponInput ? 1 : 0,
             coupon: this.couponInput,
-            subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
+            //subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
           }
           if (this.checkoutTotal == '') {
             data.finalPrice = this.subscriptionTotal;
@@ -314,7 +314,7 @@ export class SubscriptionDataComponent implements OnInit {
               finalPrice: this.checkoutTotal,
               couponApplied: this.couponInput ? 1 : 0,
               coupon: this.couponInput,
-              subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
+              //subscription_plan_id: this.selectedSubscriptionDetails?.subscription_plan_id,
             }
             this.subscriptionPlan.emit(data);
           }
