ef0feec8437bea3832faf1ebe522711f56e2971b
aareesh <aareesh@uniabroad.co.in>
2024-04-10 16:47:30 +0530

    changes

--- src/app/pages/mycertificate/mycertificate.component.ts
+++ src/app/pages/mycertificate/mycertificate.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MycertificateserviceService } from './mycertificateservice.service';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Auth/auth.service';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'uni-mycertificate',
@@ -7,17 +10,62 @@ import { MycertificateserviceService } from './mycertificateservice.service';
   styleUrls: ['./mycertificate.component.scss']
 })
 export class MycertificateComponent implements OnInit {
-
-  constructor(private service:MycertificateserviceService) { }
+certificatesList:any[]=[]
+othercirtificatecountrylist:any[]=[]
+countryname:any;
+restrict: boolean = false;
+planExpired: boolean = false;
+  constructor(private service:MycertificateserviceService,private router: Router,private authService: AuthService,private dataService: DataService,) { }
 
   ngOnInit(): void {
+    this.getCertificates();
+    this.getCertificateoOtherCountry();
+    this.checkplanExpire();
+    this.dataService.countryNameSource.subscribe((data) => {
+      this.countryname = data;
+    });
+  }
+  getCertificateoOtherCountry(){
+    var data={
+      countryid:Number(localStorage.getItem('countryId'))
+    }
+    this.service.getCertificateInOtherCountry(data).subscribe((res)=>{
+      this.othercirtificatecountrylist=res.countries
+    })
   }
   getCertificates(){
     var data={
       countryid:Number(localStorage.getItem('countryId'))
     }
-    this.service.getupcommingevent(data).subscribe((res)=>{
-      console.log(res);
+    this.service.getUserCompletedCertificate(data).subscribe((res)=>{
+      this.certificatesList=res.certificates
+    })
+  }
+  downloadCertificate(link:any){
+    if(this.planExpired){
+      this.restrict=true;
+      return;
+    }
+    window.open(link, '_blank');
+  }
+  upgradePlan(): void {
+    this.router.navigate(["/pages/subscriptions"]);
+  }
+  clearRestriction() {
+    this.restrict = false;
+  }
+  checkplanExpire(): void {
+    this.authService.getNewUserTimeLeft().subscribe((res) => {
+      let data = res.time_left;
+      let subscription_exists_status = res.subscription_details;
+      if (data.plan === "expired" || data.plan === 'subscription_expired') {
+        this.planExpired = true;
+        console.log("hi11");
+        
+      } else {
+        this.planExpired = false;
+        console.log("1111");
+      }
     })
   }
 }
--- src/app/pages/mycertificate/mycertificateservice.service.ts
+++ src/app/pages/mycertificate/mycertificateservice.service.ts
@@ -9,9 +9,15 @@ import { Observable } from 'rxjs';
 export class MycertificateserviceService {
 
   constructor(private http: HttpClient) { }
-  getupcommingevent(val: any) {
+  getUserCompletedCertificate(val: any) {
     const headers = new HttpHeaders().set("Accept", "application/json");
     return this.http.post<any>(environment.ApiUrl + "/getusercompletedcertificates",val, {
+        headers: headers,
+    });
+  }
+  getCertificateInOtherCountry(val: any) {
+    const headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.post<any>(environment.ApiUrl + "/getothercountrieswithcertificate",val, {
         headers: headers,
     });
   }
