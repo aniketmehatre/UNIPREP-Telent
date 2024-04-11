b891cc4dbda0e6f1aa93e5a246c798e862e8a2f9
aareesh <aareesh@uniabroad.co.in>
2024-04-11 17:02:51 +0530

    changescertificate

--- src/app/pages/mycertificate/mycertificate.component.ts
+++ src/app/pages/mycertificate/mycertificate.component.ts
@@ -3,7 +3,8 @@ import { MycertificateserviceService } from './mycertificateservice.service';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Auth/auth.service';
 import { DataService } from 'src/app/data.service';
-
+import { Meta } from '@angular/platform-browser';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'uni-mycertificate',
   templateUrl: './mycertificate.component.html',
@@ -15,7 +16,8 @@ othercirtificatecountrylist:any=""
 countryname:any;
 restrict: boolean = false;
 planExpired: boolean = false;
-  constructor(private service:MycertificateserviceService,private router: Router,private authService: AuthService,private dataService: DataService,) { }
+  constructor(private service:MycertificateserviceService,private router: Router,private authService: AuthService,private dataService: DataService,
+    private meta: Meta,private toast: MessageService) { }
 
   ngOnInit(): void {
     this.getCertificates();
@@ -64,5 +66,66 @@ planExpired: boolean = false;
         this.planExpired = false;
       }
     })
+  }
+  showSocialSharingList(){
+    let socialShare:any=document.getElementById("socialSharingList");
+    if(socialShare.style.display == "") {
+      socialShare.style.display = "block";
+    }
+    else {
+      socialShare.style.display = socialShare.style.display == "none" ? "block" : "none";
+    }
+  }
+  shareViaWhatsapp(link:any){
+    let url=link
+    this.meta.updateTag({ property:'og:url', content:url});
+    const shareUrl = `whatsapp://send?text=${encodeURIComponent(url)}`;
+    window.open(shareUrl, '_blank');
+  }
+  shareViaInstagram(link:any){
+    let url=link
+    this.meta.updateTag({ property:'og:url', content:url});
+    const shareUrl = `https://www.instagram.com?url=${encodeURIComponent(url)}`;
+    window.open(shareUrl, '_blank');
+  }
+  shareViaFacebook(link:any){
+    let url=link
+    this.meta.updateTag({ property:'og:url', content:url});
+    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    window.open(shareUrl, '_blank');
+  }
+  shareViaLinkedIn(link:any){
+    let url=link
+    this.meta.updateTag({ property:'og:url', content:url});
+    const shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}`;
+    window.open(shareUrl, '_blank');
+  }
+  shareViaTwitter(link:any){
+    let url=link
+    this.meta.updateTag({ property:'og:url', content:url});
+    const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`;
+    window.open(shareUrl, '_blank');
+  }
+  shareViaMail(link:any){
+    let url=link
+    this.meta.updateTag({ property:'og:url', content:url});
+    const shareUrl = `mailto:?body=${encodeURIComponent(url)}`;
+    window.open(shareUrl, '_blank');
+  }
+  copyLink(link:any){
+    const textarea = document.createElement('textarea');
+
+    // this.meta.updateTag(
+    //   { property: 'og:title', content:  this.selectedQuestionName.question},
+    // );
+    // this.meta.updateTag(
+    //   { name: 'title', content:  this.selectedQuestionName.question},
+    // );
+    textarea.textContent = link
+    document.body.append(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    this.toast.add({ severity: 'success', summary: 'Success', detail: 'Certificate link Copied' });
   }
 }
