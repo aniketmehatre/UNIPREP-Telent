5080e85767b97c4dadf7425cfdcda0fb809f24f5
vivekuniabroad <139766038+vivekuniabroad@users.noreply.github.com>
2024-06-07 19:40:55 +0530

    course list data loading and pagination

--- src/app/pages/course-list/course-list.component.ts
+++ src/app/pages/course-list/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PageFacadeService } from '../page-facade.service';
+import { CourseListService } from './course-list.service';
 
 @Component({
   selector: 'uni-course-list',
@@ -8,15 +9,42 @@ import { PageFacadeService } from '../page-facade.service';
 })
 export class CourseListComponent implements OnInit {
 
-  constructor(private pageFacade: PageFacadeService) { }
+  page:number = 1;
+  perPage: number = 50;
+  courseListData: any;
+  totalCourseCount: number = 0;
+
+  constructor(private pageFacade: PageFacadeService, private courseList: CourseListService) { }
 
   ngOnInit(): void {
+    this.getCourseLists();
   }
 
   openVideoPopup(videoLink: string) {
     this.pageFacade.openHowitWorksVideoPopup(videoLink);
   }
   
+  getCourseLists(){
+
+    let data = {
+      page: this.page,
+      perPage: this.perPage,
+    }
+
+    this.courseList.getListOfCourses(data).subscribe(response=> {
+      console.log(response.data);
+      this.courseListData = response.data;
+      this.totalCourseCount = response.total_count;
+    })
+  }
+
+  pageChange(event: any){
+
+    this.page = event.page + 1;
+    this.perPage = event.rows;
+    this.getCourseLists();
+  }
+
   closeGuidelines(){
 
   }
