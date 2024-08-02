12c6a9874431daa8f62bee5e82ad97558d28c4cd
sreenivasan-frudev <130446246+sreenivasan-frudev@users.noreply.github.com>
2024-08-02 19:41:21 +0530

    bug fix in cost of living

--- src/app/pages/job-tool/cost-of-living/cost-of-living.component.ts
+++ src/app/pages/job-tool/cost-of-living/cost-of-living.component.ts
@@ -9,7 +9,12 @@ import { City, CostOfLiving, Price } from 'src/app/@Models/cost-of-living';
   styleUrls: ['./cost-of-living.component.scss']
 })
 export class CostOfLivingComponent implements OnInit {
+
   cities: City[] = [];
+  sourceCities: City[] = [];
+  targetCities: City[] = [];
+
+  countries: { label: string, value: string }[] = [];
   form!: FormGroup;
 
   canShowComparision: boolean = false;
@@ -23,39 +28,47 @@ export class CostOfLivingComponent implements OnInit {
     this.form = this.fb.group({
       sourceCity: [null],
       targetCity: [null],
+      sourceCountry: [null],
+      targetCountry: [null],
     });
   }
 
   ngOnInit() {
     this.costOfLivingService.getCities().subscribe(res => {
+
       this.cities = res?.cities;
+      this.cities.forEach((city: City) => {
+        const country = this.countries.find((item: any) => item.label === city.country_name);
+        if (!country) {
+          this.countries.push({ label: city.country_name, value: city.country_name });
+        }
+
+      });
     });
   }
 
-  resetFunction(options: any, formcontrolName: string) {
-    options.reset();
-    this.form.get(formcontrolName)?.reset();
-  }
-
-  customFilterFunction(event: KeyboardEvent, options: any) {
-    options.filter(event);
-  }
   compare() {
     var sourceCityDetails = this.cities.find(city => city.city_id === this.form.value.sourceCity);
     var targetCityDetails = this.cities.find(city => city.city_id === this.form.value.targetCity);
     this.costOfLivingService.calculatePrices(sourceCityDetails).subscribe(response => {
       this.sourceCountryPrices = response;
-      this.sourceCountryPrices.prices.forEach((price:Price) => {
+      this.sourceCountryPrices.prices.forEach((price: Price) => {
         price.itemCount = 1;
       })
       this.costOfLivingService.calculatePrices(targetCityDetails).subscribe(response => {
         this.targetCountryPrices = response;
-        this.sourceCountryPrices.prices.forEach((price:Price) => {
+        this.sourceCountryPrices.prices.forEach((price: Price) => {
           price.itemCount = 1;
         })
         this.canShowComparision = true;
       });
     });
   }
-
+  loadCity(countryField: string) {
+    if (countryField == 'sourceCountry') {
+      this.sourceCities = this.cities.filter(city => city.country_name == this.form.get('sourceCountry')?.value);
+      return;
+    }
+    this.targetCities = this.cities.filter(city => city.country_name == this.form.get('targetCountry')?.value);
+  }
 }
