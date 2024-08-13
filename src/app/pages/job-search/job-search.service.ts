aea9d14872f9001209e859104ae7a4d68a710919
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2024-08-13 19:15:11 +0530

    job portal fix

--- src/app/pages/job-search/job-search.service.ts
+++ src/app/pages/job-search/job-search.service.ts
@@ -16,14 +16,14 @@ export class JobSearchService {
 
     searchJobs(query: any): Observable<any> {
         let params: any
-        if(query.company){
+        if (query.company) {
             params = new HttpParams()
                 .set('app_id', this.appId)
                 .set('app_key', this.appKey)
                 .set('what', query.what)
                 .set('where', query.where)
                 .set('results_per_page', query.result_per_page)
-        }else{
+        } else {
             params = new HttpParams()
                 .set('app_id', this.appId)
                 .set('app_key', this.appKey)
@@ -31,6 +31,51 @@ export class JobSearchService {
                 .set('where', query.where)
                 .set('results_per_page', query.result_per_page)
         }
+        return this.http.get(`${this.apiUrl}/${query.location}/search/${query.page}`, {params});
+    }
+
+    filter(query: any): Observable<any> {
+        let params = new HttpParams()
+            .set('app_id', this.appId)
+            .set('app_key', this.appKey);
+
+        if (query.what) {
+            params = params.set('what', query.what);
+        }
+        if (query.where) {
+            params = params.set('where', query.where);
+        }
+        if (query.result_per_page) {
+            params = params.set('results_per_page', query.result_per_page);
+        }
+        if (query.category) {
+            params = params.set('category', query.category);
+        }
+        if (query.full_time) {
+            params = params.set('full_time', query.full_time);
+        }
+        if (query.part_time) {
+            params = params.set('part_time', query.part_time);
+        }
+        if (query.contract) {
+            params = params.set('contract', query.contract);
+        }
+        if (query.permanent) {
+            params = params.set('permanent', query.permanent);
+        }
+        if (query.distance) {
+            params = params.set('distance', query.distance);
+        }
+        if (query.salary_min) {
+            params = params.set('salary_min', query.salary_min);
+        }
+        if (query.salary_max) {
+            params = params.set('salary_max', query.salary_max);
+        }
+        if (query.max_days_old) {
+            params = params.set('max_days_old', query.max_days_old);
+        }
+
         return this.http.get(`${this.apiUrl}/${query.location}/search/${query.page}`, {params});
     }
 
