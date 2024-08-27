d1ccf14316e7b8bdb494c1e602c72b42fc9e21bd
aareeshjoshy <aareesh@uniabroad.co.in>
2024-08-27 05:10:16 -0700

    changes

---
+++ src/app/domainwhitlabel.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DomainwhitlabelGuard implements CanActivate {
+  private allowedDomains = ["dev-student.uniprep.ai", "uniprep.ai", "localhost"];
+  private homePath = '/home';
+  private loginPath = '/login';
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    const currentDomain = window.location.hostname;
+    const requestedPath = state.url;
+
+    if (this.allowedDomains.includes(currentDomain)) {
+      // Allow access to all routes if the domain is allowed
+      return of(true);
+    } else {
+      // If the domain is not allowed, only prevent access to /home
+      if (requestedPath === this.homePath) {
+        this.router.navigate([this.loginPath]);
+        return of(false);
+      }
+      // Allow access to other routes even if the domain is not allowed
+      return of(true);
+    }
+  }
+}
+
