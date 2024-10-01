55f2c4afa514643a575ffbfa40198674bdb12154
vivekuniabroad <vivekkaliyaperumal1998@gmail.com>
2024-10-01 19:32:51 +0530

    cv builder location and job title suggestion completed

--- src/app/pages/job-tool/job-tool.module.ts
+++ src/app/pages/job-tool/job-tool.module.ts
@@ -35,7 +35,6 @@ import {DialogModule} from "primeng/dialog";
 import {SharedModule} from "../../shared/shared.module";
 import { TooltipModule } from 'primeng/tooltip';
 import { SkeletonModule } from 'primeng/skeleton';
-import {AutoCompleteModule} from 'primeng/autocomplete';
 
 @NgModule({
   declarations: [
@@ -77,7 +76,6 @@ import {AutoCompleteModule} from 'primeng/autocomplete';
         SharedModule,
         TooltipModule,
         SkeletonModule,
-        AutoCompleteModule,
     ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [ConfirmationService], 
