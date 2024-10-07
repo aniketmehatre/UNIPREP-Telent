231aba7cf2371c6c88a78f04fa0d8e8040885d36
Tamilselvan1996 <110588058+Tamilselvan1996@users.noreply.github.com>
2024-10-07 18:16:40 +0530

    Revert "Rebase"
    This reverts commit 7c0d66e47e1b7e4000271e10b6c3903d89fe9f3c.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+const devdomain = 'api.uniprep.ai';
+const staging='staging.uniprep.ai'
+const proddomain='uniprep.ai'
+const devurl= `https://${devdomain}/uniprepapi/public/api`;
+const stagingurl=`https://${staging}/uniprepapi/public/api`;
+const produrl=`https://${proddomain}/uniprepapi/public/api`;
+export const environment = {
+  domain:devdomain,
+  production: true,
+  ApiUrl:devurl,
+  tokenKey: 'token',
+  facebookAppId: '892925195633254',
+  linkedinId: '86b55xx7n8zomc',
+  googleCloud: {
+    apiKey: 'AIzaSyCxrgn6ZZL3IsY_3xrSqQJi_3yT_OKr-n0'
+  }
+};
