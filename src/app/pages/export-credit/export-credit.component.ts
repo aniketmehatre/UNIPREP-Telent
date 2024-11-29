8c77853370ed2283f63226ccd3796e9ef5fa4eb8
vivekuniabroad <vivekkaliyaperumal1998@gmail.com>
2024-11-29 19:23:34 +0530

    export credits button color and input disabled

--- src/app/pages/export-credit/export-credit.component.ts
+++ src/app/pages/export-credit/export-credit.component.ts
@@ -267,6 +267,12 @@ export class ExportCreditComponent implements OnInit {
     this.router.navigate(["/pages/subscriptions/upgrade-subscription"]);
   }
 
+  blockKeyboardInput(event: KeyboardEvent) :void{
+    // if (event.key !== 'Tab') { // if you want to allow any buttons you can enable the buttons here
+      event.preventDefault();
+    // }
+  }
+
   onInputChangeValue(event: any, module_id:number){
     this.totalPayableAmount = 0;
     if(this.currentCountry == "India"){
