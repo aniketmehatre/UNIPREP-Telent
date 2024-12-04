ea55f0dc73662d2733ce59f3d97066cc141b11ac
aareeshjoshy <aareesh@uniabroad.co.in>
2024-12-04 06:00:21 -0800

    chna

--- src/app/pages/founderstool/entreprenuerskillmodule/entreprenuerskillmodule.component.ts
+++ src/app/pages/founderstool/entreprenuerskillmodule/entreprenuerskillmodule.component.ts
@@ -87,7 +87,10 @@ export class EntreprenuerskillmoduleComponent implements OnInit {
   review(id:any){
     localStorage.setItem('conditionrevieworquiz','1')
     localStorage.setItem('entrpreneursubid',id)
-    this.currentModuleSlug = "entreprenuersectorproficiencytest"
+    this.currentModuleSlug = "Entreprenuer Skill Test"
+    console.log(localStorage.getItem("entrpreneursubid"));
+    console.log(localStorage.getItem("conditionrevieworquiz"));
+    
     this.router.navigate([`/pages/founderstool/${this.currentModuleSlug}/entrpreneurquiz`]);  
   }
 
