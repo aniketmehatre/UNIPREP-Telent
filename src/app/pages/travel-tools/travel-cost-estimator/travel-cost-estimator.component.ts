9e9a274cd61c17ea6b6d20ec03fa81bf722b50b6
krishnaraj-uniabrord <krishnaraj.uniabroad@gmail.com>
2025-01-07 19:40:38 +0530

    changes for travel cost estimator

--- src/app/pages/travel-tools/travel-cost-estimator/travel-cost-estimator.component.ts
+++ src/app/pages/travel-tools/travel-cost-estimator/travel-cost-estimator.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CostOfLivingService } from '../../job-tool/cost-of-living/cost-of-living.service';
 import { City } from 'src/app/@Models/cost-of-living';
+import { TravelToolsService } from '../travel-tools.service';
+import { Currency } from 'src/app/@Models/currency.model';
 
 @Component({
   selector: 'uni-travel-cost-estimator',
@@ -24,14 +26,10 @@ export class TravelCostEstimatorComponent implements OnInit {
     },
     {
       id: 4,
-      question: "Select your preferred mode of transport",
-    },
-    {
-      id: 5,
       question: "Select your experience preference"
     },
     {
-      id: 6,
+      id: 5,
       question: "Select your preferred currency"
     },
   ];
@@ -43,21 +41,49 @@ export class TravelCostEstimatorComponent implements OnInit {
   destinationFilter: string = '';
   selectedData: { [key: string]: any } = {};
   invalidClass: boolean = false;
+  trvelExperienceList: { id: number, name: string }[] = [
+    {
+      id: 1,
+      name: 'Basic'
+    },
+    {
+      id: 2,
+      name: 'Standard'
+    },
+    {
+      id: 3,
+      name: 'Luxury'
+    }
+  ];
+  currencyList: Currency[] = [];
+  recommendationData: string = '';
+  isRecommendationData: boolean = false;
 
   constructor(
-    private costOfLivingService: CostOfLivingService
+    private costOfLivingService: CostOfLivingService,
+    private travelToolsService: TravelToolsService
   ) { }
 
   ngOnInit(): void {
-    this.getCities();
+    this.selectedData = { 3: 1 };
+    this.getCityList();
+    this.getCurrencyList();
   }
 
-  getCities() {
+  getCityList() {
     this.costOfLivingService.getCities().subscribe({
       next: response => {
         this.cityList = response;
         this.departureLocationList = response;
         this.destinationLocationList = response;
+      }
+    });
+  }
+
+  getCurrencyList() {
+    this.travelToolsService.getCurrencies().subscribe({
+      next: response => {
+        this.currencyList = response;
       }
     });
   }
@@ -108,15 +134,41 @@ export class TravelCostEstimatorComponent implements OnInit {
     this.destinationFilter = "";
     this.invalidClass = false;
     // if (productId in this.selectedData) {
-      if (this.activePageIndex < this.recommendations.length - 1) {
-        this.activePageIndex++;
-      }
+    if (this.activePageIndex < this.recommendations.length - 1) {
+      this.activePageIndex++;
+    }
     // } else {
     //   this.invalidClass = true;
     // }
   }
 
   getRecommendation() {
+    // debugger
+    // let data:any = {
+    //   country: this.selectedData[1],
+    //   destination: this.selectedData[2],
+    //   duration: this.selectedData[3],
+    //   experinece: this.selectedData[4],
+    //   currency: this.selectedData[5],
+    //   mode: 'travecostestimator'
+    // }
 
+    let data: any = {
+      country: 'Germany',
+      destination: 'Canada',
+      duration: 3,
+      experience: 'Basic',
+      currency: 'CAD',
+      mode: 'travelcostestimator'
+    }
+    this.travelToolsService.getRecommendationforTravelCostEstimator(data).subscribe({
+      next: response => {
+        this.isRecommendationData = true;
+        this.recommendationData = response.response;
+      },
+      error: error => {
+        this.isRecommendationData = false;
+      }
+    });
   }
 }
--- src/app/pages/travel-tools/travel-tools.service.ts
+++ src/app/pages/travel-tools/travel-tools.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { Currency } from 'src/app/@Models/currency.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TravelToolsService {
 
-  constructor() { }
+ headers = new HttpHeaders().set("Accept", "application/json");
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCurrencies() {
+    return this.http.post<Currency[]>(environment.ApiUrl + "/getcountryandcurrency", {
+      headers: this.headers,
+    });
+  }
+
+  getRecommendationforTravelCostEstimator(data:any) {
+    return this.http.post<{response:string}>(environment.ApiUrl + "/careerplannercountrywisecurrency", data, {
+      headers: this.headers,
+    });
+  }
 }
