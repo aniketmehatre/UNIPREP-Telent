921c55b080fc15fc3a52f1cecc66cedce9cee931
vivekuniabroad <139766038+vivekuniabroad@users.noreply.github.com>
2025-01-08 20:16:03 +0530

    travel visit planner request and response page

--- src/app/pages/travel-tools/travel-visit-planner/travel-visit-planner.component.ts
+++ src/app/pages/travel-tools/travel-visit-planner/travel-visit-planner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TravelToolsService } from '../travel-tools.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'uni-travel-visit-planner',
@@ -8,81 +9,105 @@ import { TravelToolsService } from '../travel-tools.service';
 })
 export class TravelVisitPlannerComponent implements OnInit {
 
-  constructor(private travelToolService: TravelToolsService) { }
+  constructor(private travelToolService: TravelToolsService, private location: Location) { }
 
   recommendations: { id: number, question: string }[] = [
-      {
-        id: 1,
-        question: "Select Your Nationality",
-      },
-      {
-        id: 2,
-        question: "Select the country you are looking for",
-      },
-      {
-        id: 3,
-        question: "Select your residential status of ",
-      }
-    ];
-    seasons: any = [
-      {value: "Summer"},
-      {value: "Winter"},
-      {value: "Fall"},
-      {value: "Spring"}
-    ];
+    {
+      id: 1,
+      question: "Where are you planning to travel?",
+    },
+    {
+      id: 2,
+      question: "How many days will your trip be?",
+    },
+    {
+      id: 3,
+      question: "Which season or month are you planning your trip? ",
+    }
+  ];
+  seasons: any = [
+    { value: "Summer" },
+    { value: "Winter" },
+    { value: "Fall" },
+    { value: "Spring" }
+  ];
+  isRecommendation: boolean = true;
+  isResponsePage: boolean = false;
+  isSavedPage: boolean = false;
+  activePageIndex: number = 0;
+  selectedData: { [key: string]: any } = {};
+  allCountries: any = [];
+  invalidClass: boolean = false;
+  recommendationData: any = [];
+  ngOnInit(): void {
+    this.selectedData[2] = 1; //second page i need to show the days count so manually i enter the day.
+    this.getCountriesList();
+  }
 
-    activePageIndex: number = 0;
-    selectedData: { [key: string]: any } = {};
-    allCountries: any = [];
-    invalidClass: boolean = false;
-  
-    ngOnInit(): void {
-      this.getCountriesList();
+  getCountriesList(): void {
+    this.travelToolService.getCurrencies().subscribe(response => {
+      this.allCountries = response;
+    });
+  }
+
+  previous() {
+    this.invalidClass = false;
+    if (this.activePageIndex > 0) {
+      this.activePageIndex--;
     }
-  
-    getCountriesList(): void{
-      this.travelToolService.getCurrencies().subscribe( response => {
-        this.allCountries = response;
-      });
+  }
+
+  next(productId: number) {
+    this.hideWarning(productId);
+    if (!this.invalidClass) {
+      if (this.activePageIndex < this.recommendations.length - 1) {
+        this.activePageIndex++;
+      }
     }
-  
-    previous(){
+  }
+
+  getRecommendation(productId: number) {
+    this.hideWarning(productId);
+    this.isRecommendation = false;
+    this.isResponsePage = true;
+    if (!this.invalidClass) {
+      let data = {
+        destination: this.selectedData[1],
+        trip_duration: this.selectedData[2] + " days",
+        season: this.selectedData[3],
+        mode: "travel_visit_planner"
+      };
+      this.travelToolService.getChatgptRecommendations(data).subscribe(response => {
+        this.recommendationData = response.response;
+      })
+    }
+  }
+
+  hideWarning(productId: number) {
+    if (productId in this.selectedData) {
       this.invalidClass = false;
-      if (this.activePageIndex > 0) {
-        this.activePageIndex--;
-      }
+    } else {
+      this.invalidClass = true;
     }
-  
-    next(productId: number){
-      this.hideWarning(productId);
-      if(!this.invalidClass){
-        if (this.activePageIndex < this.recommendations.length - 1) {
-          this.activePageIndex++;
-        }
-      }
-    }
-  
-    getRecommendation(productId: number){
-      this.hideWarning(productId);
-      if(!this.invalidClass){
-        let data = {
-          destination: this.selectedData[1],
-          trip_duration: this.selectedData[2]+ " days",
-          season: this.selectedData[3],
-          mode: "travel_visit_planner"
-        };
-        console.log(data);
-        this.travelToolService.getChatgptRecommendations(data).subscribe(response =>{
-          console.log(response,"chatgpt response");
-        })
-      }
-    }
+  }
 
-    hideWarning(productId: number){
-      if (productId in this.selectedData) {
-        this.invalidClass = false;
-      } else {
-        this.invalidClass = true;
-      }
-    }
+  resetRecommendation() {
+    this.recommendationData = [];
+    this.isRecommendation = true;
+    this.isResponsePage = false;
+    this.isSavedPage = false;
+    this.activePageIndex = 0;
+    this.selectedData = [];
+    this.selectedData[2] = 1;
+  }
+
+  savedRecommendations() {
+    this.isRecommendation = false;
+    this.isResponsePage = false;
+    this.isSavedPage = true;
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
