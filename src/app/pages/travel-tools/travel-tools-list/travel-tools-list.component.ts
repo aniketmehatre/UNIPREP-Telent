c340d31197df1061b1b7d92be3209caacd202844
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2025-01-27 17:18:54 +0530

    fixed travel tools launch soon fix

--- src/app/pages/travel-tools/travel-tools-list/travel-tools-list.component.ts
+++ src/app/pages/travel-tools/travel-tools-list/travel-tools-list.component.ts
@@ -13,41 +13,41 @@ export class TravelToolsListComponent implements OnInit {
        
         image: "https://api.uniprep.ai/uniprepapi/storage/app//public/icon/modules/job-interview.svg",
         url: "/pages/travel-tools/travel-visa",
-        launch_soon: true
+        launch_soon: false
       },
       {
         title: "Travel Cost Estimator",
        
         image: "https://api.uniprep.ai/uniprepapi/storage/app//public/icon/modules/job-interview.svg",
         url: "/pages/travel-tools/travel-cost-estimator",
-        launch_soon: true
+        launch_soon: false
       },
       {
         title: "Travel Visit Planner",
        
         image: "https://api.uniprep.ai/uniprepapi/storage/app//public/icon/modules/job-interview.svg",
         url: "/pages/travel-tools/travel-visit-planner",
-        launch_soon: true
+        launch_soon: false
       },
       {
         title: "Travel Packing Planner",
        
         image: "https://api.uniprep.ai/uniprepapi/storage/app//public/icon/modules/job-interview.svg",
         url: "/pages/travel-tools/travel-packing-planner",
-        launch_soon: true
+        launch_soon: false
       },
       {
         title: "Trip Length Finder",
         image: "https://api.uniprep.ai/uniprepapi/storage/app//public/icon/modules/job-interview.svg",
         url: "/pages/travel-tools/trip-length-finder",
-        launch_soon: true
+        launch_soon: false
       },
       {
         title: "Travel Glossary",
        
         image: "https://api.uniprep.ai/uniprepapi/storage/app//public/icon/modules/job-interview.svg",
         url: "/pages/travel-tools/travel-glossary",
-        launch_soon: true
+        launch_soon: false
       },
       {
         title: "Cost of Living Comparision",
