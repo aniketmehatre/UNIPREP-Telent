c1c4197f341f0cf46be4c1501e2d2f4bded7ea03
Saurabh Nakkarike <nakkarike@gmail.com>
2025-02-07 11:45:50 +0530

    Login page not working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './Auth/login/login.component';
 
 export const appRoutes: Routes = [
   // Public routes
   { path: '', redirectTo: '/landing', pathMatch: 'full' }, // Default route
   { path: 'landing', component: LandingComponent }, // Landing outside of auth
-
+  { path: 'login', component: LoginComponent },
   // Lazy load Auth Module
   { path: 'auth', loadChildren: () => import('./Auth/auth.module').then(m => m.AuthModule) },
 
