d30e835020d4408aec6166a502799ea8f61a82d1
Saurabh Nakkarike <nakkarike@gmail.com>
2025-02-10 12:41:49 +0530

    Dropdown Check

--- src/app/pages/job-tool/salary-converter/salary-converter.component.ts
+++ src/app/pages/job-tool/salary-converter/salary-converter.component.ts
@@ -1,168 +1,165 @@
-import { ChangeDetectorRef, Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { SalaryConverterService } from "./salary-converter.service";
-import { Location } from "@angular/common";
-import { Router } from "@angular/router";
-import { LocationService } from "../../../location.service";
-import { PlanService } from "../../../shared/plan.service";
-import { AuthService } from "../../../Auth/auth.service";
-import { CommonModule } from "@angular/common";
-import { DialogModule } from "primeng/dialog";
-import { DropdownModule } from "primeng/dropdown";
-import { FormsModule } from "@angular/forms";
-import { InputNumberModule } from "primeng/inputnumber";
+import { ChangeDetectorRef, Component, OnInit, ViewEncapsulation } from "@angular/core"
+import { SalaryConverterService } from "./salary-converter.service"
+import { Location } from "@angular/common"
+import { Router } from "@angular/router"
+import { LocationService } from "../../../location.service"
+import { PlanService } from "../../../shared/plan.service"
+import { AuthService } from "../../../Auth/auth.service"
+import { CommonModule } from "@angular/common"
+import { DialogModule } from "primeng/dialog"
+import { DropdownModule } from "primeng/dropdown"
+import { FormsModule } from "@angular/forms"
+import { InputNumberModule } from "primeng/inputnumber"
+import { CarouselModule } from "primeng/carousel"
+import { ButtonModule } from "primeng/button"
+
 @Component({
-  selector: "uni-salary-converter",
-  templateUrl: "./salary-converter.component.html",
-  styleUrls: ["./salary-converter.component.scss"],
-  standalone: true,
-  imports: [CommonModule, DialogModule, DropdownModule, FormsModule, InputNumberModule],
-  encapsulation: ViewEncapsulation.None,
+	selector: "uni-salary-converter",
+	templateUrl: "./salary-converter.component.html",
+	styleUrls: ["./salary-converter.component.scss"],
+	standalone: true,
+	imports: [CommonModule,ButtonModule, CarouselModule, DialogModule, DropdownModule, FormsModule, InputNumberModule],
+	encapsulation: ViewEncapsulation.None,
 })
 export class SalaryConverterComponent implements OnInit {
-  salary: any;
-  selectedCurrencyCode: string = "INR";
-  selectedCountryName: any;
-  selectedCountryCode: any;
-  selectedToCurrencyCode: string = "INR";
-  selectedToCountryCode: any;
-  selectedToCountryName: any;
-  taxData: any;
-  fromCountry: any;
-  toCountry: any;
-  countries: any[] = [];
-  rates: any;
-  statementText: any;
-  inHomeCurrency: any;
-  isPPPCardVisible: boolean = false;
-  planExpired: boolean = false;
-  restrict: boolean = false;
-  ehitlabelIsShow: boolean = true;
-  imagewhitlabeldomainname: any;
-  orgnamewhitlabel: any;
-  orglogowhitelabel: any;
-  salaries: { satement: string }[] = [
-    { satement: "Earning INR 50,000 in the India is equivalent to earning INR 1,78,571.43 in the United Arab Emirates." },
-    { satement: "Earning INR 70,000 in the India is equivalent to earning INR 2,12,500 in the United Kingdom." },
-    { satement: "Earning INR 50,000 the India is equivalent to earning INR 16,071.43 in the Canada." },
-    { satement: "Earning INR 70,000 in the India is equivalent to earning INR 1,90,000 in the France." },
-    { satement: "Earning INR 1,00,000 in the India is equivalent to earning INR 3,57,142.86 in the United States." },
-  ];
-  responsiveOptions: { breakpoint: string; numVisible: number; numScroll: number }[];
-  get fromValue() {
-    return this.taxData[0];
-  }
-  get toValue() {
-    return this.taxData[1];
-  }
+	salary: any
+	selectedCurrencyCode: string = "INR"
+	selectedCountryName: any
+	selectedCountryCode: any
+	selectedToCurrencyCode: string = "INR"
+	selectedToCountryCode: any
+	selectedToCountryName: any
+	taxData: any
+	fromCountry: any
+	toCountry: any
+	countries: any[] = []
+	rates: any
+	statementText: any
+	inHomeCurrency: any
+	isPPPCardVisible: boolean = false
+	planExpired: boolean = false
+	restrict: boolean = false
+	ehitlabelIsShow: boolean = true
+	imagewhitlabeldomainname: any
+	orgnamewhitlabel: any
+	orglogowhitelabel: any
+	salaries: { satement: string }[] = [{ satement: "Earning INR 50,000 in the India is equivalent to earning INR 1,78,571.43 in the United Arab Emirates." }, { satement: "Earning INR 70,000 in the India is equivalent to earning INR 2,12,500 in the United Kingdom." }, { satement: "Earning INR 50,000 the India is equivalent to earning INR 16,071.43 in the Canada." }, { satement: "Earning INR 70,000 in the India is equivalent to earning INR 1,90,000 in the France." }, { satement: "Earning INR 1,00,000 in the India is equivalent to earning INR 3,57,142.86 in the United States." }]
+	responsiveOptions: { breakpoint: string; numVisible: number; numScroll: number }[]
+	get fromValue() {
+		return this.taxData[0]
+	}
+	get toValue() {
+		return this.taxData[1]
+	}
 
-  constructor(private salaryConverterService: SalaryConverterService, private _location: Location, private authService: AuthService, private router: Router, private locationService: LocationService, private cdr: ChangeDetectorRef) {
-    this.responsiveOptions = [
-      {
-        breakpoint: "1024px",
-        numVisible: 3,
-        numScroll: 3,
-      },
-      {
-        breakpoint: "768px",
-        numVisible: 2,
-        numScroll: 2,
-      },
-      {
-        breakpoint: "560px",
-        numVisible: 1,
-        numScroll: 1,
-      },
-    ];
-  }
+	constructor(private salaryConverterService: SalaryConverterService, private _location: Location, private authService: AuthService, private router: Router, private locationService: LocationService, private cdr: ChangeDetectorRef) {
+		this.responsiveOptions = [
+			{
+				breakpoint: "1024px",
+				numVisible: 3,
+				numScroll: 3,
+			},
+			{
+				breakpoint: "768px",
+				numVisible: 2,
+				numScroll: 2,
+			},
+			{
+				breakpoint: "560px",
+				numVisible: 1,
+				numScroll: 1,
+			},
+		]
+	}
 
-  ngOnInit(): void {
-    this.locationService.getImage().subscribe((imageUrl) => {
-      this.orglogowhitelabel = imageUrl;
-    });
-    this.locationService.getOrgName().subscribe((orgname) => {
-      this.orgnamewhitlabel = orgname;
-    });
-    this.imagewhitlabeldomainname = window.location.hostname;
-    if (this.imagewhitlabeldomainname === "dev-student.uniprep.ai" || this.imagewhitlabeldomainname === "uniprep.ai" || this.imagewhitlabeldomainname === "localhost") {
-      this.ehitlabelIsShow = true;
-    } else {
-      this.ehitlabelIsShow = false;
-    }
-    this.checkplanExpire();
-    this.salaryConverterService.getCountries().subscribe((data) => {
-      this.countries = data;
-    });
-  }
+	ngOnInit(): void {
+		this.locationService.getImage().subscribe((imageUrl) => {
+			this.orglogowhitelabel = imageUrl
+		})
+		this.locationService.getOrgName().subscribe((orgname) => {
+			this.orgnamewhitlabel = orgname
+		})
+		this.imagewhitlabeldomainname = window.location.hostname
+		if (this.imagewhitlabeldomainname === "dev-student.uniprep.ai" || this.imagewhitlabeldomainname === "uniprep.ai" || this.imagewhitlabeldomainname === "localhost") {
+			this.ehitlabelIsShow = true
+		} else {
+			this.ehitlabelIsShow = false
+		}
+		this.checkplanExpire()
+		this.salaryConverterService.getCountries().subscribe((data) => {
+			this.countries = data
+		})
+	}
 
-  isShowPlanExpiredDialog: boolean = false;
-  convert(): void {
-    // this.planService.checkPlanIsExpired().subscribe((isExpired) => {
-    //   if (isExpired) {
-    //     this.planExpired = isExpired;
-    //     this.restrict = isExpired;
-    //     this.isShowPlanExpiredDialog = true
-    //     this.cdr.detectChanges();
-    //     return;
-    //   } else {
-    //
-    //   }
-    // });
-    if (this.planExpired) {
-      this.restrict = true;
-      return;
-    }
-    this.isShowPlanExpiredDialog = false;
-    if (this.fromCountry && this.toCountry && this.salary) {
-      const fromPpp = this.selectedCountryCode;
-      const toPpp = this.selectedToCountryCode;
-      let req = {
-        codes: `${fromPpp},${toPpp}`,
-        amt: this.salary,
-      };
-      this.salaryConverterService.getTaxData(req).subscribe((resp: any) => {
-        this.taxData = resp.data;
-        this.statementText = resp.statement;
-        this.inHomeCurrency = resp.inHomeCurrency;
-        this.isPPPCardVisible = true;
-      });
-    }
-  }
+	isShowPlanExpiredDialog: boolean = false
+	convert(): void {
+		// this.planService.checkPlanIsExpired().subscribe((isExpired) => {
+		//   if (isExpired) {
+		//     this.planExpired = isExpired;
+		//     this.restrict = isExpired;
+		//     this.isShowPlanExpiredDialog = true
+		//     this.cdr.detectChanges();
+		//     return;
+		//   } else {
+		//
+		//   }
+		// });
+		if (this.planExpired) {
+			this.restrict = true
+			return
+		}
+		this.isShowPlanExpiredDialog = false
+		if (this.fromCountry && this.toCountry && this.salary) {
+			const fromPpp = this.selectedCountryCode
+			const toPpp = this.selectedToCountryCode
+			let req = {
+				codes: `${fromPpp},${toPpp}`,
+				amt: this.salary,
+			}
+			this.salaryConverterService.getTaxData(req).subscribe((resp: any) => {
+				this.taxData = resp.data
+				this.statementText = resp.statement
+				this.inHomeCurrency = resp.inHomeCurrency
+				this.isPPPCardVisible = true
+			})
+		}
+	}
 
-  onCountryChange(event: any) {
-    this.selectedCountryCode = event.value.countryCode;
-    this.selectedCurrencyCode = event.value.currencyCode;
-    this.selectedCountryName = event.value.countryName;
-    this.isPPPCardVisible = false;
-    this.taxData = [];
-  }
+	onCountryChange(event: any) {
+		this.selectedCountryCode = event.value.countryCode
+		this.selectedCurrencyCode = event.value.currencyCode
+		this.selectedCountryName = event.value.countryName
+		this.isPPPCardVisible = false
+		this.taxData = []
+	}
 
-  onCountryToChange(event: any) {
-    this.selectedToCountryCode = event.value.countryCode;
-    this.selectedToCurrencyCode = event.value.currencyCode;
-    this.selectedToCountryName = event.value.countryName;
-    this.isPPPCardVisible = false;
-    this.taxData = [];
-  }
+	onCountryToChange(event: any) {
+		this.selectedToCountryCode = event.value.countryCode
+		this.selectedToCurrencyCode = event.value.currencyCode
+		this.selectedToCountryName = event.value.countryName
+		this.isPPPCardVisible = false
+		this.taxData = []
+	}
 
-  goBack() {
-    this._location.back();
-  }
-  checkplanExpire(): void {
-    this.authService.getNewUserTimeLeft().subscribe((res) => {
-      let data = res.time_left;
-      let subscription_exists_status = res.subscription_details;
-      if (data.plan === "expired" || data.plan === "subscription_expired" || subscription_exists_status.subscription_plan == "Student") {
-        this.planExpired = true;
-      } else {
-        this.planExpired = false;
-      }
-    });
-  }
-  upgradePlan(): void {
-    this.router.navigate(["/pages/subscriptions"]);
-  }
+	goBack() {
+		this._location.back()
+	}
+	checkplanExpire(): void {
+		this.authService.getNewUserTimeLeft().subscribe((res) => {
+			let data = res.time_left
+			let subscription_exists_status = res.subscription_details
+			if (data.plan === "expired" || data.plan === "subscription_expired" || subscription_exists_status.subscription_plan == "Student") {
+				this.planExpired = true
+			} else {
+				this.planExpired = false
+			}
+		})
+	}
+	upgradePlan(): void {
+		this.router.navigate(["/pages/subscriptions"])
+	}
 
-  clearRestriction() {
-    this.restrict = false;
-  }
+	clearRestriction() {
+		this.restrict = false
+	}
 }
