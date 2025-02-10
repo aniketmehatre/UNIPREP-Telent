1de782ea90a69a2a5de10dcb6839608c55667670
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2025-02-10 07:19:59 +0530

    fixed unused imports and update lint issues

--- src/app/Auth/login/login.component.ts
+++ src/app/Auth/login/login.component.ts
@@ -1,51 +1,53 @@
-import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from "@angular/core";
 import {
-  FormGroup,
-  Validators,
-  FormBuilder,
-} from "@angular/forms";
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { FormGroup, Validators, FormBuilder } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../auth.service";
 import { MessageService } from "primeng/api";
 import { SubSink } from "subsink";
 import { DataService } from "src/app/data.service";
-import {DashboardService} from "../../pages/dashboard/dashboard.service";
-import {GoogleLoginProvider, SocialAuthService} from "@abacritt/angularx-social-login";
-import {environment} from "@env/environment";
-import {LocalStorageService} from "ngx-localstorage";
-import {Observable} from "rxjs/internal/Observable";
-import {FacebookLoginProvider} from "angularx-social-login";
-// import {FacebookService} from "ngx-facebook";
-import {LocationService} from "../../location.service";
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { environment } from "@env/environment";
+import { LocalStorageService } from "ngx-localstorage";
+import { LocationService } from "../../location.service";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  @ViewChild('button2') button2!: ElementRef;
+  @ViewChild("button2") button2!: ElementRef;
   private subs = new SubSink();
   loginForm: any = FormGroup;
   submitted: boolean = false;
   show: boolean = true;
-  password: string = 'password';
-  isDisabled:boolean=false;
-  locationData: any
+  password: string = "password";
+  isDisabled: boolean = false;
+  locationData: any;
   imageUrlWhitelabel: string | null = null;
-  domainname:any;
-  domainnamecondition:any;
+  domainname: any;
+  domainNameCondition: any;
+  ipURL: string = "https://api.ipify.org?format=json";
   constructor(
-    private service: AuthService, private formBuilder: FormBuilder, private route: Router,
-    private toast: MessageService, private dataService: DataService, private el: ElementRef,
+    private service: AuthService,
+    private formBuilder: FormBuilder,
+    private route: Router,
+    private toast: MessageService,
+    private dataService: DataService,
     private locationService: LocationService,
     private authService: SocialAuthService,
-    private storage: LocalStorageService, 
-  ) { }
+    private storage: LocalStorageService
+  ) {}
 
   linkedInCredentials = {
     clientId: environment.linkedinId,
     redirectUrl: "http://localhost:4200/pages/dashboard",
-    scope: "r_liteprofile%20r_emailaddress%20w_member_social" // To read basic user profile data and email
+    scope: "r_liteprofile%20r_emailaddress%20w_member_social", // To read basic user profile data and email
   };
 
   button1Clicked() {
@@ -56,113 +58,134 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.subs.unsubscribe();
   }
 
-  showPassword(): void{
-    if (this.password === 'password') {
-      this.password = 'text';
+  showPassword(): void {
+    if (this.password === "password") {
+      this.password = "text";
       this.show = false;
     } else {
-      this.password = 'password';
+      this.password = "password";
       this.show = true;
     }
   }
 
   ngOnInit() {
     if (this.service.isTokenValid()) {
-      this.route.navigate(['/pages/dashboard']); // Redirect to dashboard
+      this.route.navigate(["/pages/dashboard"]); // Redirect to dashboard
     }
-    this.locationService.getImage().subscribe(imageUrl => {
+    this.locationService.getImage().subscribe((imageUrl) => {
       this.imageUrlWhitelabel = imageUrl;
     });
-    this.domainnamecondition=window.location.hostname
-    if ( this.domainnamecondition === "dev-student.uniprep.ai" ||  this.domainnamecondition === "uniprep.ai" ||  this.domainnamecondition === "localhost") {
-      this.domainname="main"
-    }else{
-      this.domainname="sub"
+    this.domainNameCondition = window.location.hostname;
+    if (
+      this.domainNameCondition === "dev-student.uniprep.ai" ||
+      this.domainNameCondition === "uniprep.ai" ||
+      this.domainNameCondition === "localhost"
+    ) {
+      this.domainname = "main";
+    } else {
+      this.domainname = "sub";
     }
-    this.dataService.loggedInAnotherDevice('none');
-    fetch('https://ipapi.co/json/').then(response => response.json()).then(data => {
-      this.locationData = data
-    });
+    this.dataService.loggedInAnotherDevice("none");
+    fetch(this.ipURL)
+      .then((response) => response.json())
+      .then((data) => {
+        this.locationData = data;
+      });
     this.authService.authState.subscribe((user) => {
       let data = {
-        email: user.email
-      }
-      this.service.isExist(data).subscribe(data => {
-        if (data == 'Exist'){
-          this.service.gmailLogin(user).subscribe(data => {
-            if(data.status == 'error'){
-              this.toast.add({ severity: 'error', summary: 'Error', detail: data });
-              return;
-            }          
-            this.service.saveToken(data.token);
-            this.storage.set(environment.tokenKey, data.token);
-            this.service.getMe().subscribe((data) => {
-                  this.loadCountryList(data);
-                  this.toast.add({ severity: 'success', summary: 'Success', detail: 'Login' });
-                  this.route.navigate(['/pages/dashboard']);
-
-            });
-          },
-          (error: any) => {
-            this.toast.add({
-              severity: "error",
-              summary: "Error",
-              detail: error,
-            });
+        email: user.email,
+      };
+      this.service.isExist(data).subscribe((data) => {
+        if (data == "Exist") {
+          this.service.gmailLogin(user).subscribe(
+            (data) => {
+              if (data.status == "error") {
+                this.toast.add({
+                  severity: "error",
+                  summary: "Error",
+                  detail: data,
+                });
+                return;
+              }
+              this.service.saveToken(data.token);
+              this.storage.set(environment.tokenKey, data.token);
+              this.service.getMe().subscribe((data) => {
+                this.loadCountryList(data);
+                this.toast.add({
+                  severity: "success",
+                  summary: "Success",
+                  detail: "Login",
+                });
+                this.route.navigate(["/pages/dashboard"]);
+              });
+            },
+            (error: any) => {
+              this.toast.add({
+                severity: "error",
+                summary: "Error",
+                detail: error,
+              });
+            }
+          );
+        } else {
+          this.toast.add({
+            severity: "info",
+            summary: "Info",
+            detail: "Email not exist , Try Register",
           });
-        }else{
-          this.toast.add({ severity: 'info', summary: 'Info', detail: 'Email not exist , Try Register' });
         }
       });
-      //var socialUser = user;
-      //this.loggedIn = (user != null);
     });
     this.loginForm = this.formBuilder.group({
       email: ["", [Validators.required, Validators.email]],
       password: ["", [Validators.required]],
-      domain_type:[this.domainname]
+      domain_type: [this.domainname],
     });
-    this.subs.sink = this.service.selectloggedIn$().subscribe(loggedIn => {
+    this.subs.sink = this.service.selectloggedIn$().subscribe((loggedIn) => {
       if (!loggedIn) {
-        return
+        return;
       }
 
       this.dataService.showTimerInHeader(loggedIn);
-      this.subs.sink = this.service!.getMe().subscribe((data) => {          
+      this.subs.sink = this.service!.getMe().subscribe((data) => {
         this.service.saveToken(data.userdetails[0].token);
         this.loadCountryList(data);
-        this.subs.sink = this.service.selectMessage$().subscribe(message => {
-          if (message == 'Login Success') {
+        this.subs.sink = this.service.selectMessage$().subscribe((message) => {
+          if (message == "Login Success") {
             let req = {
               userId: data.userdetails[0].user_id,
               location: this.locationData.city,
               country: this.locationData.country_name,
-            }
-            this.locationService.sendSessionData(req, "login").subscribe(response => {
-              //console.log('addtrack', response);
+            };
+            this.locationService
+              .sendSessionData(req, "login")
+              .subscribe((response) => {
+              });
+            this.toast.add({
+              severity: "success",
+              summary: "Success",
+              detail: message,
             });
-            this.toast.add({ severity: 'success', summary: 'Success', detail: message });
           }
         });
-        let url = localStorage.getItem('previousUrl') || '';
-        if(url) {
+        let url = localStorage.getItem("previousUrl") || "";
+        if (url) {
           this.route.navigate([url]);
-        }
-        else {
-          this.route.navigate(['/pages/dashboard']);
+        } else {
+          this.route.navigate(["/pages/dashboard"]);
         }
       });
     });
   }
 
   countryLists: any;
-  loadCountryList(data: any){
-    this.locationService.getCountry().subscribe(countryList => {
+  loadCountryList(data: any) {
+    this.locationService.getCountry().subscribe((countryList) => {
       this.countryLists = countryList;
       this.countryLists.forEach((element: any) => {
-        let cont = Number(data.userdetails[0].selected_country)
+        let cont = Number(data.userdetails[0].selected_country);
         if (element.id == cont) {
-          localStorage.setItem('countryId', cont.toString());
+          localStorage.setItem("countryId", cont.toString());
           this.dataService.changeCountryName(element.country);
           this.dataService.changeCountryFlag(element.flag);
           this.dataService.changeCountryId(element.id);
@@ -182,23 +205,8 @@ export class LoginComponent implements OnInit, OnDestroy {
     }
     this.service.canDisableSignIn.next(true);
     this.service.login(this.loginForm.value);
-    this.service.canDisableSignIn.subscribe(res=>{
-      this.isDisabled=res;
+    this.service.canDisableSignIn.subscribe((res) => {
+      this.isDisabled = res;
     });
-
   }
-
-  // loginWithFacebook(){
-  //   this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
-  // }
-  //
-  // loginWithLinkedIn(){
-  //     this.ngxLinkedinService.signIn().subscribe(user => {
-  //       console.info('signIn', user);
-  //     });
-  //   window.location.href = `https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=${
-  //       this.linkedInCredentials.clientId
-  //   }&redirect_uri=${this.linkedInCredentials.redirectUrl}&scope={this.linkedInCredentials.scope}`;
-  // }
-
 }
