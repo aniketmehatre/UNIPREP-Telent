68b95c4479faa5a3bf0ccb5fbe497b0fcf046bd9
Saurabh Nakkarike <nakkarike@gmail.com>
2025-02-11 15:20:02 +0530

    Assessment module fix

--- src/app/pages/assessment/assessment.component.ts
+++ src/app/pages/assessment/assessment.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DialogModule } from 'primeng/dialog';
+import { CardModule } from 'primeng/card';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
     selector: 'uni-assessment',
     templateUrl: './assessment.component.html',
     styleUrls: ['./assessment.component.scss'],
     standalone: true,
-    imports: [CommonModule]
+    imports: [CommonModule, RouterModule, DialogModule, CardModule, PaginatorModule]
 })
 export class AssessmentComponent implements OnInit {
 
