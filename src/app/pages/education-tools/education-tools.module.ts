326fcd9fcefa6cf77de183cfd4146b3ff0815c06
Saurabh Nakkarike <nakkarike@gmail.com>
2025-02-12 12:18:53 +0530

    Education Tools Fix

--- src/app/pages/education-tools/education-tools.module.ts
+++ src/app/pages/education-tools/education-tools.module.ts
@@ -3,60 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { EducationToolsRoutingModule } from './education-tools-routing.module';
 import { EducationToolsComponent } from './education-tools.component';
-import { EducationToolsListComponent } from './education-tools-list/education-tools-list.component';
-import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CarouselModule } from 'primeng/carousel';
-
-import { ButtonModule } from 'primeng/button';
-import { PoliticianInsightsComponent } from './politician-insights/politician-insights.component';
-import { PaginatorModule } from 'primeng/paginator';
-import { CardModule } from 'primeng/card';
-import { DialogModule } from 'primeng/dialog';
-import {SharedModule} from "../../shared/shared.module";
-import { InputTextModule } from 'primeng/inputtext';
-import { CountryInsightsComponent } from './country-insights/country-insights.component';
-import { CountryInsightsSubmoduleComponent } from './country-insights/country-insights-submodule/country-insights-submodule.component';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { StudentBudgetPlannerComponent } from './student-budget-planner/student-budget-planner.component';
-import { WealthleaderreadansComponent } from '../founderstool/wealthleaderreadans/wealthleaderreadans.component';
-import { WealthleaderslistComponent } from '../founderstool/wealthleaderslist/wealthleaderslist.component';
-import { UniCompareComponent } from './uni-compare/uni-compare.component';
-import { GlobalEdufitComponent } from './global-edufit/global-edufit.component';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { EduLoanCompareComponent } from './edu-loan-compare/edu-loan-compare.component';
-import { SkeletonModule } from 'primeng/skeleton';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    CountryInsightsSubmoduleComponent,
-    WealthleaderreadansComponent,
-    PoliticianInsightsComponent,
-    CountryInsightsComponent,
-    CourseNavigatorComponent,
-    EducationToolsComponent,
-    EducationToolsListComponent,
-    RouterModule,
-    EducationToolsRoutingModule,
-    StudentBudgetPlannerComponent,
-    WealthleaderslistComponent,
-    FormsModule,
-    CarouselModule,
-    GlobalEdufitComponent,
-    UniCompareComponent,
-    EduLoanCompareComponent,
-    ButtonModule,
-    PaginatorModule,
-    CardModule,
-    DialogModule,
-    SharedModule,
-    InputTextModule,
-    SelectButtonModule,
-    ReactiveFormsModule,
-    MultiSelectModule,
-    SkeletonModule
-  ]
+	imports: [
+		CommonModule,
+		EducationToolsRoutingModule,
+		EducationToolsComponent // Only need the main component since others are lazy loaded
+	]
 })
 export class EducationToolsModule { }
