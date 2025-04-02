f62e20bb9b083c0b9c38022d626d6f4c13b96a99
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2025-04-02 22:51:53 +0530

    vocabulary fixed

--- src/app/pages/language-hub/vocabulary/vocabulary.component.ts
+++ src/app/pages/language-hub/vocabulary/vocabulary.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from "@angular/core"
-import { CommonModule } from "@angular/common"
-import { LanguageArrayGlobalService } from "../language-array-global.service"
-import { PageFacadeService } from "../../page-facade.service"
-import { Location } from "@angular/common"
-import { LanguageHubService } from "../language-hub.service"
-import { FormsModule } from "@angular/forms"
-import { SkeletonModule } from "primeng/skeleton"
-import { TooltipModule } from "primeng/tooltip"
-import { ButtonModule } from "primeng/button"
-import { MultiSelectModule } from "primeng/multiselect"
-import { CarouselModule } from "primeng/carousel"
-import { InputGroupModule } from "primeng/inputgroup"
-import { InputGroupAddonModule } from "primeng/inputgroupaddon"
-import { RouterModule } from "@angular/router"
+import {Component, OnInit} from "@angular/core"
+import {CommonModule, Location} from "@angular/common"
+import {LanguageArrayGlobalService} from "../language-array-global.service"
+import {PageFacadeService} from "../../page-facade.service"
+import {LanguageHubService} from "../language-hub.service"
+import {FormsModule} from "@angular/forms"
+import {SkeletonModule} from "primeng/skeleton"
+import {TooltipModule} from "primeng/tooltip"
+import {ButtonModule} from "primeng/button"
+import {MultiSelectModule} from "primeng/multiselect"
+import {CarouselModule} from "primeng/carousel"
+import {InputGroupModule} from "primeng/inputgroup"
+import {InputGroupAddonModule} from "primeng/inputgroupaddon"
+import {ActivatedRoute, RouterModule} from "@angular/router"
+import {TranslateViewService} from "../translate-view/translate-view.service";
 
 @Component({
 	selector: "app-vocabulary",
@@ -26,38 +26,82 @@ export class VocabularyComponent implements OnInit {
 	searchTerm: string = ""
 	isPlaying1: boolean = false
 	words: any
+	id: string | null = null;
 
-	groupedWords: { letter: string; words: { word: string; translation: string }[] }[] = []
+	playingStates: { [key: string]: boolean } = {};
 
-	constructor(private languageArrayGlobalService: LanguageArrayGlobalService, private pageFacade: PageFacadeService, private location: Location, private languageHubService: LanguageHubService) {}
+	groupedWords: { letter: string; words: { id: number, english_words: string; searched_words: string }[] }[] = []
+
+
+	constructor(private languageArrayGlobalService: LanguageArrayGlobalService, private pageFacade: PageFacadeService,
+				private location: Location, private languageHubService: LanguageHubService,
+				private translateViewService: TranslateViewService,
+				private route: ActivatedRoute) {
+	}
 
 	getFormattedValues(): string {
 		return this.languageArrayGlobalService.getItems().join(" -> ")
 	}
 
 	ngOnInit() {
+		this.route.paramMap.subscribe((params: any) => {
+			this.id = params.get('id'); // Get the value of "id"
+		});
 		this.getVocabularyRecord()
 	}
 
 	getVocabularyRecord() {
-		this.languageHubService.getVocabulary(null).subscribe((_res) => {
+		let req = {
+			language_id: this.id
+		}
+		this.languageHubService.getVocabulary(req).subscribe((_res) => {
 			this.words = _res
 			this.groupWordsByAlphabet()
 		})
 	}
 
 	onClickLetter(letter: any) {
-		this.languageHubService.getVocabulary(letter).subscribe((_res) => {
+		let req = {
+			language_id: this.id,
+			letters: letter
+		}
+		this.languageHubService.getVocabulary(req).subscribe((_res) => {
 			this.words = _res
 			this.groupWordsByAlphabet()
 		})
+	}
+
+	onClickLetterNew(character?: any) {
+		character = character.toUpperCase();
+
+		// Filter words starting with the specified character
+		this.groupedWords = [{
+			letter: character,
+			words: this.words.filter((word: any) => word.english_words.startsWith(character))
+		}].filter(group => group.words.length > 0);
+	}
+
+	filterWords() {
+		if (!this.searchTerm) {
+			// If no search term, reset to the full list
+			this.groupWordsByAlphabet();
+		} else {
+			const searchTerm = this.searchTerm.toUpperCase(); // Normalize the search term for case-insensitive comparison
+			// Filter the alphabet to include only those letters that match the starting character of the search term
+			const relevantAlphabets = this.alphabet.filter(letter => letter.startsWith(searchTerm[0]));
+
+			this.groupedWords = relevantAlphabets.map(letter => ({
+				letter,
+				words: this.words.filter((word: any) => word.english_words.toUpperCase().startsWith(searchTerm))
+			})).filter(group => group.words.length > 0);
+		}
 	}
 
 	groupWordsByAlphabet() {
 		this.groupedWords = this.alphabet
 			.map((letter) => ({
 				letter,
-				words: this.words.filter((word: any) => word.word.startsWith(letter)),
+				words: this.words.filter((word: any) => word.english_words.startsWith(letter)),
 			}))
 			.filter((group) => group.words.length > 0)
 	}
@@ -69,5 +113,28 @@ export class VocabularyComponent implements OnInit {
 	goToHome(event: any) {
 		this.languageArrayGlobalService.removeItem(this.languageArrayGlobalService.getItems().length - 1)
 		this.location.back()
+	}
+
+	synthesizeSpeech(content: any, id: any) {
+		this.translateViewService.synthesize(content, 'hi').subscribe((response: any) => {
+			const audioContent = response.audioContent
+			const audioUrl = "data:audio/mp3;base64," + audioContent
+			const audio = new Audio(audioUrl)
+			audio.play()
+			audio.addEventListener("ended", () => {
+				this.isPlaying1 = false
+			})
+			this.playingStates[id] = false;
+		})
+	}
+
+	togglePlayPause(id: number, content: any) {
+		if (this.playingStates[id]) {
+			this.playingStates[id] = false;
+			this.synthesizeSpeech(content, id)
+		} else {
+			this.playingStates[id] = true;
+			this.synthesizeSpeech(content, id)
+		}
 	}
 }
