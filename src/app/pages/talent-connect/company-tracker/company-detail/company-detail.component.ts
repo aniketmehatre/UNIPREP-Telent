c59111e553a21f263e2a3b3331544b023e2634f1
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2025-04-10 18:07:44 +0530

    update merge conflicts

--- src/app/pages/talent-connect/company-tracker/company-detail/company-detail.component.ts
+++ src/app/pages/talent-connect/company-tracker/company-detail/company-detail.component.ts
@@ -20,46 +20,57 @@ export class CompanyDetailComponent implements OnInit, OnChanges {
   @Input() showInfo: boolean = true;
   showChat: boolean = false;
 
-  companyData = {
+  companyInfo = {
     name: 'UNIABROAD Technology Pvt. Ltd.',
-    foundedYear: '2019',
-    logo: 'assets/uniabroad-logo.png',
+    founded: 2019,
     headquartersLocation: 'Mysore, Karnataka',
     companyType: 'Startup',
     companySize: '10-49 Employees',
     sizeCategory: 'Small enterprises',
-    globalPresence: ['India', 'United Kingdom'],
     industryType: 'Primary',
-    officialWebsite: 'www.uniabroad.co.in',
+    globalPresence: ['India', 'United Kingdom'],
+    websiteUrl: 'www.uniabroad.co.in',
     linkedInProfile: 'https://www.linkedin.com/company/example-corp/',
-    about: [
-      "We're a company dedicated to helping students achieve their dream of studying abroad. Our journey began in 2019, when our founders recognized the need for a reliable and comprehensive overseas education company. Since then, we've been working tirelessly to guide students in the process of applying to and studying at top universities around the world.",
-      "We're proud of the work we've done so far, and we're excited to continue making a positive impact on the lives of students around the world."
-    ].join('\n\n'),
-    workLifeBalancePolicies: [
-      'Flexible Work Arrangements',
-      'Health Insurance',
-      'Childcare Assistance',
-      'Learning & Growth',
-      'Remote Work Options'
-    ],
-    employeeBenefits: [
-      'Professional Development',
-      'Workplace Perks',
-      'Employee Recognition & Rewards',
-      'Flexible Work Arrangements'
-    ],
-    workLocation: 'On-Site',
-    hiringStages: [
-      'Application Submission',
-      'Initial Screening',
-      'Technical Interview',
-      'HR Interview',
-      'Final Decision'
-    ],
-    hiringTimeframe: '2-4 Weeks',
-    interviewFormat: 'Online & In-Person'
+    logo: 'assets/uniabroad-logo.png',
+    about: 'We\'re a company dedicated to helping students achieve their dream of studying abroad. Our journey began in 2019, when our founders recognized the need for a reliable and comprehensive overseas education company. Since then, we\'ve been working tirelessly to guide students in the process of applying to and studying at top universities around the world.\nWe\'re proud of the work we\'ve done so far, and we\'re excited to continue making a positive impact on the lives of students around the world.'
   };
+  companyDetails!: Company;
+
+  constructor(private talentConnectService: TalentConnectService) {
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes) {
+      this.init();
+    }
+  }
+
+  init() {
+    this.talentConnectService.getCompanyDetails(this.companyId).subscribe({
+      next: data => {
+        this.companyDetails = data[0];
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
+  followCompany() {
+    this.talentConnectService.followCompany(this.companyId).subscribe({
+      next: response => {
+        if (response.success) {
+          this.companyDetails.followed = 1;
+        }
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 
 }
 
