14aca813c3c0f4c6a4b08f6869d9f470160c35ee
kandhanramalingam <kandhanramalingam1@gmail.com>
2025-05-06 13:53:17 +0530

    Changes

---
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'uni-contact-us',
+  standalone: true,
+  imports: [
+      CommonModule
+  ],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.scss'
+})
+export class ContactUsComponent {
+
+}
