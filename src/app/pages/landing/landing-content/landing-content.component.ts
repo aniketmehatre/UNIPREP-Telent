2aafb6fe253977f922b096772e7975bf4871ac74
Mohit-Agastya1 <mohits.uniabroad@gmail.com>
2025-06-25 22:59:10 -0400

    design bug fix

--- src/app/pages/landing/landing-content/landing-content.component.ts
+++ src/app/pages/landing/landing-content/landing-content.component.ts
@@ -270,10 +270,30 @@ export class LandingContentComponent implements OnInit {
   
     if (this.isFromUUID) {
       this.uuidCardData = {
-        name: 'John Doe',
-        program: 'AI Career Prep',
-        message: 'You have been invited via a special link!'
+        companyLogo: '', 
+        title: 'Senior UI/UX Designer',
+        postedDate: '19–02–2025',
+        vacancies: 50,
+        applied: 10,
+        video: 'Not Mandatory',
+        startDate: '19–02–2025',
+        deadline: '25–02–2025',
+        location: 'Mysore, India',
+        workMode: 'Onsite',
+        experience: '0–50',
+        position: 'UI Designer',
+        salary: '₹50,000/mo',
+        level: 'Mid Level',
+        degree: "Bachelor's",
+        industry: 'Tech Industry',
+        overview: `We are looking for a creative and detail-oriented UI/UX Designer to join our team at UNIABROAD. The ideal candidate will be responsible for designing user-friendly, engaging, and visually appealing interfaces for our digital platforms, ensuring a seamless user experience for students and stakeholders.`,
+        responsibilities: [
+          'Design and implement intuitive, user-centered interfaces for web and mobile applications.',
+          'Conduct user research and usability testing to understand pain points and improve UI/UX designs.',
+          'Develop wireframes, prototypes, and mockups that effectively communicate design ideas.'
+        ]
       };
+      
       return;
     }
     
