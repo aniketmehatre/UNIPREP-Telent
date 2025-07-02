8e5d9a5fba299c0bb20a5e71807eabc1695c60d8
krishnaraj-uniabrord <krishnaraj.uniabroad@gmail.com>
2025-07-02 18:48:00 +0530

    changes

--- src/app/pages/talent-connect/talent-connect.service.ts
+++ src/app/pages/talent-connect/talent-connect.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { map } from 'rxjs';
 import { removeExtraResponse } from '../prompt';
-import { EmployeeConnectProfileRes } from 'src/app/@Models/employee-connect-profile';
+import { EmployeeConnectProfile, EmployeeConnectProfileRes } from 'src/app/@Models/employee-connect-profile';
 import { Departments } from 'src/app/@Models/user-profile.model';
 
 @Injectable({
@@ -17,124 +17,137 @@ export class TalentConnectService {
         'x-rapidapi-host': 'currency-conversion-and-exchange-rates.p.rapidapi.com',
         'x-rapidapi-key': 'd08adbb963msh135bd172e57612cp19ee92jsna8b68088b175'
     });
-
+    _employerProfileData!: EmployeeConnectProfile | null;
     constructor(private http: HttpClient) { }
 
-
-    getCountries() {
-        return this.http.post<any>(environment.ApiUrl + "/getcountryandcurrency", {
-            headers: this.headers
-        });
-    }
-
-    getCityCountries(search?: string) {
-        return this.http.get<any>(environment.ApiUrl + `/getworldcitiescountry?search=${search ?? ''}`);
+    //Profile Creation 
+    getMyProfileData() {
+        const request$ = this.http.get<EmployeeConnectProfileRes>(environment.ApiUrl + "/getstudentprofiles", { headers: this.headers }).pipe(
+            map(response => {
+                if (response?.data?.length > 0) {
+                    this._employerProfileData = response.data[0];
+                }
+                return response;
+            }),
+            catchError(error => {
+                this._employerProfileData = null;
+                return throwError(() => error);
+            })
+        );
+        return request$;
     }
 
     getAiSummaryByMode(mode: string, data: any) {
         return this.http.post<any>(environment.ApiUrl + `/yourprofileaigenerate`, { mode: mode, ...data });
     }
 
+    getMyProfileDropDownValues() {
+        return this.http.get<any>(environment.ApiUrl + "/getyourprofiledropdownvalues", { headers: this.headers });
+    }
+
+    profileCreationBasicInfo(formData: any) {
+        return this.http.post<{ message: String, student_id: number }>(`${environment.ApiUrl}/studentprofile/basic`, formData);
+    }
+
+    profileCreationEducationInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/education`, formData);
+    }
+
+    profileCreationExperienceInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/experience`, formData);
+    }
+
+    profileCreationCareerInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/career`, formData);
+    }
+
+    profileCreationCertificateInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/certificate`, formData);
+    }
+
+    profileCreationLanguageInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/language`, formData);
+    }
+
+    profileCreationNetworkInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/network`, formData);
+    }
+
+    profileCreationReferenceInfo(formData: any) {
+        return this.http.post(`${environment.ApiUrl}/studentprofile/reference`, formData);
+    }
+
+    getStudentProfilesUsingId(id: string) {
+        let params = new HttpParams().set('student_id', id);
+        return this.http.get<any>(environment.ApiUrl + "/getstudentprofilesusingid", { params });
+    }
+
+    getCountries() {
+        return this.http.post<any>(environment.ApiUrl + "/getcountryandcurrency", { headers: this.headers });
+    }
+
+    getCityCountries(search?: string) {
+        return this.http.get<any>(environment.ApiUrl + `/getworldcitiescountry?search=${search ?? ''}`);
+    }
+
     getEasyApplyWorkLocationList() {
-        return this.http.get<any>(environment.ApiUrl + "/employer/easyappyworklocations", {
-            headers: this.headers
-        });
+        return this.http.get<any>(environment.ApiUrl + "/employer/easyappyworklocations", { headers: this.headers });
     }
 
     //Easy-Apply
     getJobListDropdown() {
-        return this.http.get<any>(
-            environment.ApiUrl + "/employer/easyappydropdownlist",
-            { headers: this.headers });
+        return this.http.get<any>(environment.ApiUrl + "/employer/easyappydropdownlist", { headers: this.headers });
     }
 
     getJobLocationList() {
-        return this.http.get<any>(
-            environment.ApiUrl + "/easyappyworklocations",
-            { headers: this.headers });
+        return this.http.get<any>(environment.ApiUrl + "/easyappyworklocations", { headers: this.headers });
     }
 
     getJobList(data: any) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/getuserJobList",
-            data,
-            { headers: this.headers }
-        );
+        return this.http.post<any>(environment.ApiUrl + "/getuserJobList", data, { headers: this.headers });
     }
 
     getJobDetails(id: number) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/showuserJobs", { id: id },
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/showuserJobs", { id: id }, { headers: this.headers });
     }
 
     applyJob(id: number) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/saveeasyapplyjob", { job_id: id },
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/saveeasyapplyjob", { job_id: id }, { headers: this.headers });
     }
 
     getAppliedJobList(data: any) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/getusertracklist", data,
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/getusertracklist", data, { headers: this.headers });
     }
 
     getJobTrackerDetail(id: number) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/showuserTrack", { id: id },
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/showuserTrack", { id: id }, { headers: this.headers });
     }
 
     sendMessage(formData: FormData) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/sendMessage", formData,
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/sendMessage", formData, { headers: this.headers });
     }
 
     getMessage(data: { job_id: number }) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/getMessage", data,
-            { headers: this.headers });
-    }
-
-    //My Profile
-    getMyProfileDropDownValues() {
-        return this.http.get<any>(
-            environment.ApiUrl + "/getyourprofiledropdownvalues",
-            { headers: this.headers });
-    }
-
-    getMyProfileData() {
-        return this.http.get<EmployeeConnectProfileRes>(
-            environment.ApiUrl + "/getstudentprofiles",
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/getMessage", data, { headers: this.headers });
     }
 
     getDepartmentData() {
-        return this.http.get<any>(
-            environment.ApiUrl + "/getdepartments",
-            { headers: this.headers });
-    }
-
-    submitProfile(formData: any): Observable<any> {
-        return this.http.post(`${environment.ApiUrl}/createstudentprofile`, formData);
-    }
-
-    updateProfile(formData: any): Observable<any> {
-        return this.http.post(`${environment.ApiUrl}/updatestudentprofile`, formData);
+        return this.http.get<any>(environment.ApiUrl + "/getdepartments", { headers: this.headers });
     }
 
     getJobAiSummary(data: any) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/jobChatAiGenerate", data,
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/jobChatAiGenerate", data, { headers: this.headers });
+    }
+
+    getUUID(jobId: any) {
+        const formData = new FormData();
+        formData.append('job_id', jobId);
+        const headers = new HttpHeaders().set("Accept", "application/json");
+        return this.http.post<any>(`${environment.ApiUrlEmployer}/getJobShareLink`, formData, { headers: headers });
     }
 
     getCompanyChatAiSummary(data: any) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/airephrase", data,
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/airephrase", data, { headers: this.headers });
     }
 
     followCompany(companyId: any, followStatus: number): Observable<any> {
@@ -145,61 +158,47 @@ export class TalentConnectService {
         return this.http.post<any>(environment.ApiUrl + "/shortlistcompany", formData, { headers: headers });
     }
 
-    // company connect
     getTalentConnectCompanies(formValues: any) {
         const headers = new HttpHeaders().set("Accept", "application/json");
-        return this.http.post<any>(
-            environment.ApiUrl + "/gettalentconnectcompanies", formValues,
-            { headers: headers });
+        return this.http.post<any>(environment.ApiUrl + "/gettalentconnectcompanies", formValues, { headers: headers });
     }
-
 
     getCompanyDetails(id: any) {
         const formData = new FormData();
         formData.append("id", id);
         const headers = new HttpHeaders().set("Accept", "application/json");
-        return this.http.post<any>(
-            environment.ApiUrl + "/getcompanydetails", formData,
-            { headers: headers });
+        return this.http.post<any>(environment.ApiUrl + "/getcompanydetails", formData, { headers: headers });
     }
 
-    // getcompanytypes
     getCompanyTypes() {
         const headers = new HttpHeaders().set("Accept", "application/json");
         return this.http.get<any>(environment.ApiUrl + "/getcompanytypes", { headers: headers });
     }
 
-    // getindustrytypes
     getIndustryTypes() {
         const headers = new HttpHeaders().set("Accept", "application/json");
         return this.http.get<any>(environment.ApiUrl + "/getindustrytypes", { headers: headers });
     }
 
-    // getcompanysize
     getCompanySizes() {
         const headers = new HttpHeaders().set("Accept", "application/json");
         return this.http.get<any>(environment.ApiUrl + "/getcompanysize", { headers: headers });
     }
 
-    // globalPresence
     globalPresence() {
         const headers = new HttpHeaders().set("Accept", "application/json");
         return this.http.post<any>(environment.ApiUrlEmployer + "/globalPresence", {}, { headers: headers });
     }
 
-    // getcitywithflag
     getCityWithFlag() {
         const headers = new HttpHeaders().set("Accept", "application/json");
         return this.http.post<any>(environment.ApiUrlEmployer + "/getcitywithflag", {}, { headers: headers });
     }
 
     getCurrencies() {
-        return this.http.get<any>(environment.ApiUrl + "/currenciesList", {
-            headers: this.headers
-        });
+        return this.http.get<any>(environment.ApiUrl + "/currenciesList", { headers: this.headers });
     }
 
-    // get chat message
     getChatMessageForCompanyConnect(companyId: any) {
         const formData = new FormData();
         formData.append("companyid", companyId);
@@ -216,7 +215,6 @@ export class TalentConnectService {
         const headers = new HttpHeaders().set("Accept", "application/json");
         return this.http.post<any>(environment.ApiUrl + "/sendcompanyconnectusermessage", formData, { headers: headers });
     }
-
 
     //getcompanytracker
     getCompanyTracker(formValues: any) {
@@ -240,13 +238,6 @@ export class TalentConnectService {
         return this.http.post<any>(environment.ApiUrl + "/getsendmessagecompanytracker", formValues, { headers: headers });
     }
 
-    getStudentProfilesUsingId(id: string) {
-        let params = new HttpParams()
-            .set('student_id', id);
-        return this.http.get<any>(
-            environment.ApiUrl + "/getstudentprofilesusingid", { params },);
-    }
-
     getAiEvaluationSummary(formData: any) {
         return this.http.post<any>(environment.ApiUrl + "/getIntegratedRecom", formData, { headers: this.headers }).pipe(
             map(res => ({ response: removeExtraResponse(res.response), profile_percent: res.profile_percent })) // Process response before returning
@@ -254,9 +245,7 @@ export class TalentConnectService {
     }
 
     getCompanyConnectAiSummary(formData: any) {
-        return this.http.post<any>(
-            environment.ApiUrl + "/companyChatAiGenerate", formData,
-            { headers: this.headers });
+        return this.http.post<any>(environment.ApiUrl + "/companyChatAiGenerate", formData, { headers: this.headers });
     }
 
     getCurrencyConverter(base: string = 'USD', symbols: string = 'INR', start: string = '2025-05-11', end: string = '2025-05-12') {
@@ -273,38 +262,4 @@ export class TalentConnectService {
         return this.http.get<Departments[]>(environment.ApiUrl + `/getdepartments`);
     }
 
-    //Profile Creation
-    profileCreationBasicInfo(formData: any) {
-        return this.http.post<{ message: String, student_id: number }>(`${environment.ApiUrl}/studentprofile/basic`, formData);
-    }
-
-    profileCreationEducationInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/education`, formData);
-    }
-
-    profileCreationExperienceInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/experience`, formData);
-    }
-    profileCreationCareerInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/career`, formData);
-    }
-    profileCreationCertificateInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/certificate`, formData);
-    }
-    profileCreationLanguageInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/language`, formData);
-    }
-    profileCreationNetworkInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/network`, formData);
-    }
-    profileCreationReferenceInfo(formData: any) {
-        return this.http.post(`${environment.ApiUrl}/studentprofile/reference`, formData);
-    }
-
-    getUUID(jobId: any) {
-        const formData = new FormData();
-        formData.append('job_id', jobId);
-        const headers = new HttpHeaders().set("Accept", "application/json");
-        return this.http.post<any>(`${environment.ApiUrlEmployer}/getJobShareLink`, formData, { headers: headers });
-    }
 }
