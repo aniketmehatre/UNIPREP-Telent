bf677b41c9707f9caea5638dd13d2659ef1320d5
aareeshjoshy <aareesh@uniabroad.co.in>
2025-07-03 11:57:26 +0530

    changes

--- src/app/pages/landing-institute/landing-institute.service.ts
+++ src/app/pages/landing-institute/landing-institute.service.ts
@@ -29,7 +29,7 @@ export class LandingInstituteService {
         formData.append('name', formValue.name);
         formData.append('email', formValue.email);
         formData.append('phone', formValue.phone_number);
-        formData.append('phone_country_code', formValue.phone_country_code);
+        formData.append('phonenumber', formValue.phonenumber);
         formData.append('password', formValue.password);
         formData.append('password_confirmation', formValue.password_confirmation);
         formData.append('country_id', formValue.country_id);
