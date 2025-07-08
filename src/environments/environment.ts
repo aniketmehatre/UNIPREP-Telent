ddf87c6933c8f162cc1a88882174e26e113ae7e6
aareeshjoshy <aareesh@uniabroad.co.in>
2025-07-08 13:58:42 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,10 +20,10 @@ const dev_institute = 'https://dev-institutes.uniprep.ai';
 const prod_institute = 'https://institutes.uniprep.ai';
 
 export const environment = {
-    domain: prod_domain,
+    domain: dev_domain,
     maintenanceMode: false,
     production: false,
-    ApiUrl: prod_url,
+    ApiUrl: dev_url,
     ApiUrlEmployer: prod_url + '/employer',
     employerDomain: prod_employer_url,
     tokenKey: 'token',
