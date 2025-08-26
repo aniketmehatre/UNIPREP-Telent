4de0f24966b93c1a6d55b8ca2d935ae2d58052c2
krishnaraj-uniabrord <krishnaraj.uniabroad@gmail.com>
2025-08-26 17:19:47 +0530

    changes for currency

--- src/app/pages/talent-connect/employee-profile/employee-profile.component.ts
+++ src/app/pages/talent-connect/employee-profile/employee-profile.component.ts
@@ -782,7 +782,7 @@ export class EmployeeProfileComponent implements OnInit, OnDestroy {
       career_preference_preferred_employment_type: response.careerPreference?.preferred_employment_type || [],
       career_preference_preferred_workplace_type: response.careerPreference?.preferred_workplace_type || [],
       career_preference_expected_salary: response.careerPreference?.expected_salary || null,
-      career_preference_currency_id: response.careerPreference?.currency_id || null,
+      career_preference_currency_id: response.careerPreference?.currency_id || this.currentCurrenyId,
     });
 
     this.additionalNotesForm.patchValue({
