69dc29d6d26098f89e339c8045386ee3b92ffcb0
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2025-09-09 10:34:50 +0530

    fixed issue in sharedcomponents

--- src/app/@shared/components/index.ts
+++ src/app/@shared/components/index.ts
@@ -1,13 +1,13 @@
-import {NgModule} from "@angular/core";
-import {AvatarComponent} from "./avatar/avatar.component";
-import {ResultAccordionComponent} from "./result-accordion/result-accordion.component";
-import {AccordionModule} from "primeng/accordion";
-import {CommonModule, NgForOf, NgSwitchCase} from "@angular/common";
-import {TooltipModule} from "primeng/tooltip";
+import { NgModule } from "@angular/core";
+import { AvatarComponent } from "./avatar/avatar.component";
+import { ResultAccordionComponent } from "./result-accordion/result-accordion.component";
+import { AccordionModule } from "primeng/accordion";
+import { CommonModule, NgForOf, NgSwitchCase } from "@angular/common";
+import { TooltipModule } from "primeng/tooltip";
 
 @NgModule({
     declarations: [AvatarComponent, ResultAccordionComponent],
     imports: [CommonModule, AccordionModule, NgForOf, NgSwitchCase, TooltipModule],
     exports: [AvatarComponent, ResultAccordionComponent]
 })
-export class SharedComponents {}``
+export class SharedComponents { }
