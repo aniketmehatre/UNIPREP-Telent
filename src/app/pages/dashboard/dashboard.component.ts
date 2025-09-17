8b5bd75abfeef4cb5c8030d208729f25f1f155c5
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2025-09-17 09:26:42 +0530

    updated dashboard YT link

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -360,7 +360,7 @@ export class DashboardComponent implements OnInit, OnChanges, OnDestroy {
 			// } else {
 			// 	this.userBasedVideo = 'https://www.youtube.com/embed/AAXUZ0z5bl0?si=xAFiTKSQGhHrQ9iE'
 			// }
-			this.userBasedVideo = 'https://www.youtube.com/embed/opXvLo1OS_o?si=m4-GWVTIIk1yXwuK'
+			this.userBasedVideo = 'https://www.youtube.com/embed/5MSn3pbD11A?si=4mxp9G9rdbkBZDPH'
 			this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.userBasedVideo);
 		})
 	}
