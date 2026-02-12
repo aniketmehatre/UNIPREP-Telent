8e43af71e1cc58d2374594cc9614f455f48b7403
Vivek BM <72000961+vivekbasvivek@users.noreply.github.com>
2026-02-12 09:41:09 +0530

    fixex cost of living

--- src/app/pages/job-tool/cost-of-living/cost-of-living.service.ts
+++ src/app/pages/job-tool/cost-of-living/cost-of-living.service.ts
@@ -1,32 +1,55 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { City, CostOfLiving, CurrencyConvert, GoodWithIcon } from "src/app/@Models/cost-of-living";
+import {
+  City,
+  CostOfLiving,
+  CurrencyConvert,
+  GoodWithIcon,
+} from "src/app/@Models/cost-of-living";
 import { environment } from "@env/environment";
 
 @Injectable({
-    providedIn: "root",
+  providedIn: "root",
 })
 export class CostOfLivingService {
-    constructor(private http: HttpClient) { }
-    inrRate: any;
-    calculatePrices(cityDetails: any) {
-        const headers = new HttpHeaders().set("X-RapidApi-Key", "09dff7af89msh193627b79d4f967p12d174jsn905ad1176841");
-        if (cityDetails.city_name == null) {
-            return this.http.get<CostOfLiving>(`https://cost-of-living-and-prices.p.rapidapi.com/prices?country_name=${cityDetails.country_name}`, { headers: headers });
-        }
-        return this.http.get<CostOfLiving>(`https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=${cityDetails.city_name}&country_name=${cityDetails.country_name}`, { headers: headers });
+  constructor(private http: HttpClient) {}
+  inrRate: any;
+  calculatePrices(cityDetails: any) {
+    // const headers = new HttpHeaders().set("X-RapidApi-Key", "8221e7a6d4msh41f8f8c02735020p1c8dddjsn6fdf6667711d");
+    const headers = new HttpHeaders().set(
+      "X-RapidApi-Key",
+      "8221e7a6d4msh41f8f8c02735020p1c8dddjsn6fdf6667711d",
+    );
+    if (cityDetails.city_name == null) {
+      return this.http.get<CostOfLiving>(
+        `https://cost-of-living-and-prices.p.rapidapi.com/prices?country_name=${cityDetails.country_name}`,
+        { headers: headers },
+      );
     }
-    getCities() {
-        const headers = new HttpHeaders().set("Accept", "application/json");
-        return this.http.post<City[]>(environment.ApiUrl + "/getcitywithflag", { headers: headers });
-    }
-    currencyConvert(data: any) {
-        const headers = new HttpHeaders().set("Accept", "application/json");
-        return this.http.post<CurrencyConvert>(environment.ApiUrl + "/currencyConvert", data, { headers: headers });
-    }
-    getItemIconsList() {
-        const headers = new HttpHeaders().set("Accept", "application/json");
-        return this.http.post<GoodWithIcon[]>(environment.ApiUrl + "/getitemicons", { headers: headers });
-    }
-
+    return this.http.get<CostOfLiving>(
+      `https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=${cityDetails.city_name}&country_name=${cityDetails.country_name}`,
+      { headers: headers },
+    );
+  }
+  getCities() {
+    const headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.post<City[]>(environment.ApiUrl + "/getcitywithflag", {
+      headers: headers,
+    });
+  }
+  currencyConvert(data: any) {
+    const headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.post<CurrencyConvert>(
+      environment.ApiUrl + "/currencyConvert",
+      data,
+      { headers: headers },
+    );
+  }
+  getItemIconsList() {
+    const headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.post<GoodWithIcon[]>(
+      environment.ApiUrl + "/getitemicons",
+      { headers: headers },
+    );
+  }
 }
